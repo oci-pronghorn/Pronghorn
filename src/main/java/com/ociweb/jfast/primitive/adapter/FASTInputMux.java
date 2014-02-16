01db7865b684e0ffac5cac304871ed7a128864b7
nathantippy <nathan@tippy.name>
2014-02-16 08:34:16 -0600

    Rough in the details of how the input mux will work

--- src/main/java/com/ociweb/jfast/primitive/adapter/FASTInputMux.java
+++ src/main/java/com/ociweb/jfast/primitive/adapter/FASTInputMux.java
@@ -3,152 +3,187 @@ package com.ociweb.jfast.primitive.adapter;
 import com.ociweb.jfast.primitive.FASTInput;
 
 
-/**
- * Reads from all the available inputs and fills the buffer with the "fastest" and
- * "most agreed upon" values from the feed.
- * 
- * "Fastest" and "most agreed upon" is determined by the the first n inputs to
- * agree on the provided the data where n is set on startup.
- * 
- * For example if n is set to 2 and 5 inputs are provided. And further if the first
- * two inputs to respond do not agree and the third does agree with one of the first two 
- * at that time the result will be returned.  The remaining two inputs will have the data
- * loaded to provide a hot fail over but it will not be used.
- *    
- * 
- */
+
 public class FASTInputMux implements FASTInput {
 
-	final FASTInput[] sources;
-	final int sourceCount;
+	final FASTInput sourceA;
+	final FASTInput sourceB;
+	final FASTInput sourceC;
+	
+	int headA, tailA;
+	int headB, tailB;
+	int headC, tailC;
+	long posA, posB, posC;
+	
 	byte[] targetBuffer;
 	byte[] working;
-	int[] heads;
-	int[] tails;
-	long[] pos;
-	
+		
 	long sendingPos;
 
-	final int matches;
 	final int FETCH_SIZE = 256;
 	final int FETCH_MASK = FETCH_SIZE-1;
 	
-	public FASTInputMux(FASTInput[] sources, int matches) {
-		this.sources=sources;
-		this.sourceCount = sources.length;
-		this.working = new byte[FETCH_SIZE*sourceCount];
-		//these never wrap and are moved back down to 0 when possible
-		this.heads = new int[sourceCount];
-		this.tails = new int[sourceCount];//exclusive
-		this.pos = new long[sourceCount];
-		this.matches = matches;
+	public FASTInputMux(FASTInput sourceA, 
+			             FASTInput sourceB, 
+			             FASTInput sourceC) {
+		this.sourceA = sourceA;
+		this.sourceB = sourceB;
+		this.sourceC = sourceC;
+				
+		this.working = new byte[FETCH_SIZE*3];
 	}
+	
+	//TODO: return the failure state for external manager (must be another thread!).
+	//TODO: external manager can startup new FASTInput and add it here once its running.
+	
+	
 	
 	@Override
 	public int fill(int offset, int count) {
+		//
+		//toss out any old data from slow feeds that are catching up.
+		//
+		if (posA<sendingPos && headA<tailA) {
+			long step = Math.min(sendingPos-posA,tailA-headA);
+			headA+=step;
+			posA+=step;
+			//reset back to beginning because we do not loop
+			if (headA==tailA) {
+				headA=tailA=0;
+			}
+		}
+		if (posB<sendingPos && headB<tailB) {
+			long step = Math.min(sendingPos-posB,tailB-headB);
+			headB+=step;
+			posB+=step;
+			//reset back to beginning because we do not loop
+			if (headB==tailB) {
+				headB=tailB=0;
+			}
+		}
+		if (posC<sendingPos && headC<tailC) {
+			long step = Math.min(sendingPos-posC,tailC-headC);
+			headC+=step;
+			posC+=step;
+			//reset back to beginning because we do not loop
+			if (headC==tailC) {
+				headC=tailC=0;
+			}
+		}
+		//
+		//load available data			
+		//
+		tailA += sourceA.fill(tailA, Math.min( (FETCH_SIZE - (FETCH_MASK & tailA)), count));
+		tailB += sourceB.fill(tailB, Math.min( (FETCH_SIZE - (FETCH_MASK & tailB)), count));
+		tailC += sourceC.fill(tailC, Math.min( (FETCH_SIZE - (FETCH_MASK & tailC)), count));
 		
-		//int lastByte = Integer.MAX_VALUE;
-		//int matchCount = 0;
-		
-		long checker = 0; //TODO: sources may not be > 8
-		
-		int i = sources.length;
-		while (--i>=0) {
-			int maxFill = Math.min( (FETCH_SIZE - (FETCH_MASK&tails[i])), count);
-			int result = 0==maxFill ? 0 : sources[i].fill(tails[i], maxFill);
-			
-			tails[i]+=result;
-			//each source is up to date for what is has at the moment.
-			//
-			//toss out any old data from slow feeds that are catching up.
-			if (pos[i]<sendingPos && heads[i]<tails[i]) {
-				long step = Math.min(sendingPos-pos[i],tails[i]-heads[i]);
-				heads[i]+=step;
-				pos[i]+=step;
-				//reset back to beginning because we do not loop
-				if (heads[i]==tails[i]) {
-					heads[i]=tails[i]=0;
+		//
+		//check for matching values, TODO: Far from the best implementation but good enough for now.
+		//
+		int tmp = 0;
+		if (posA == sendingPos) {
+			if (posB == sendingPos) {
+				if (posC == sendingPos) {
+					//ready to use A B and C
+					while(working[headA] == working[headB] && 
+						   working[headB] == working[headC] &&
+						   headA<tailA && 
+						   headB<tailB && 
+						   headC<tailC) {
+						
+						targetBuffer[offset++] = working[headA];
+						headA++;
+						headB++;
+						headC++;						
+						tmp++;
+					}
+					
+					if (working[headA] == working[headB]) {
+						//A and B match
+						int r = copyCheckedValues(offset, Math.min(tailA-headA, tailB-headB), headA, headB);
+						headA += r;
+						headB += r;
+						tmp+=r;
+						
+						//sourceC.reset(); //we need to do this but not block on the connection wait.
+						//we also need the position after the reset.
+
+					} else {
+						if (working[headB] == working[headC]) {
+							//B and C match
+							int r = copyCheckedValues(offset, Math.min(tailB-headB, tailC-headC), headB, headC);
+							headB += r;
+							headC += r;
+							tmp+=r;
+							
+						} else {
+							if (working[headA] == working[headC]) {
+								//A and C match
+								int r = copyCheckedValues(offset, Math.min(tailA-headA, tailC-headC), headA, headC);
+								headA += r;
+								headC += r;
+								tmp+=r;
+							} 
+						}						
+					}
+				} else {
+					//ready to use A and B but NO C
+					int r = copyCheckedValues(offset, Math.min(tailA-headA, tailB-headB), headA, headB);
+					headA += r;
+					headB += r;
+					tmp+=r;
+				}				
+			} else {
+				//no B so C is now required or the check can not be made.
+				if (posC == sendingPos) {
+				    //ready to use A and C but NO B
+					int r = copyCheckedValues(offset, Math.min(tailA-headA, tailC-headC), headA, headC);
+					headA += r;
+					headC += r;
+					tmp+=r;
 				}
 			}
-			
-			// 0 1 2 3 4 5
-			//  x   x   x
-            //    x   x
-			
-			
-			//xor the even ones, and xor the odd ones. everything should be zero.
-			
-			
-			//now check for matching data
-			if (pos[i]==sendingPos && heads[i]<tails[i]) {
-				checker = (checker<<8)|working[heads[i]];
-				
-//				//head has the value we are after
-//				byte datum = working[heads[i]];
-//				if (datum == lastByte) {
-//					matchCount++;					
-//				} else {
-//					if (matchCount==0) {
-//						//switch to this new value if we have not found any matches so far
-//						lastByte=datum;						
-//					} 
-//				}
-				
-				heads[i]++;
-				pos[i]++;
-				
-				
-				
-			}
-			
-			
+		} else {
+			//unable to use A
+			if (posB == sendingPos & posC==sendingPos) {
+				//ready to use B and C but NO A
+				int r = copyCheckedValues(offset, Math.min(tailB-headB, tailC-headC), headB, headC);
+				headB += r;
+				headC += r;
+				tmp+=r;
+			} 
 		}
-		
-		//TODO: how to deal with this long to know about match count?
-		
-		//assuming that 1 is always a failure
-		//rotate 8bits for each of the inputs-1
-		//and xor the results together.
-		
-		// 0 1 2 3
-		// 1 2 3 0
-		
-		// 0 1 2 3
-		// 3 0 1 2 
-		
-		// 0 1 2 3
-		// 2 3 0 1  
-				
-		//6 zeros which is 3! and we need 3
-		
-		//0 1 2
-		//2 0 1
-		
-		//0 1 2
-		//1 2 0
-		
-		//2 zeros which is 2! and we need 2
-		
-		
-				
-		
-		
-		return 1;
+		//unable to check anything at this time
+		return tmp;
+	}
+
+	private int copyCheckedValues(int offset, int len, int i, int j) {
+		int c = 0;
+		while (--len>0 && working[i]==working[j]) {
+			targetBuffer[offset++] = working[i];						
+			i++;
+			j++;
+			c++;
+		}
+		return c;
 	}
 
 	@Override
 	public void init(byte[] targetBuffer) {
 		this.targetBuffer = targetBuffer;
+		this.sourceA.init(working);
+		this.sourceB.init(working);
+		this.sourceC.init(working);
 		
-		int i = sources.length;
-		while (--i>=0) {
-			sources[i].init(working);
-			int offset = i*FETCH_SIZE;
-			heads[i] = offset;
-		    tails[i] = offset;
-		    //pos is zero for the first byte of stream
-		}
+		int offset = 0;
+		headA = tailA = offset;
 		
+		offset+=FETCH_SIZE;
+		headB = tailB = offset;
+		
+		offset+=FETCH_SIZE;
+		headC = tailC = offset;
+				
 	}
 
 }
---
+++ src/test/java/com/ociweb/jfast/primitive/adapter/FASTInputMuxTest.java
@@ -0,0 +1,24 @@
+package com.ociweb.jfast.primitive.adapter;
+
+public class FASTInputMuxTest {
+
+	//Not sure yet how to fully test this so this class is in flux as I figure it out.
+	
+	public void newTestingThoughts() {
+		
+		//build a known binary stream for usage in the tests.
+		
+		//use the known binary stream to build 3 FASTInput objects
+		
+		//wrap FASTInput with a TestFASTInput allowing the injection of errors
+		//Two failure states (BadData, NoResponse)
+		
+		//confirm that the injected error is detected.
+		
+		//How does FASTInputMux recover from failures?
+		
+		//Single monitor thread for all inputs in JVM.
+		
+	}
+	
+}
