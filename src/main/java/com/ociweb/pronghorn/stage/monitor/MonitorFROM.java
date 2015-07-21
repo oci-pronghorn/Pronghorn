81bba7484d4022f7271b5ee4d64c72d70155875a
Nathan Tippy <nathan@tippy.name>
2015-07-21 14:05:25 -0500

    Removed runtime dependency on XML to support compact1

--- src/main/java/com/ociweb/pronghorn/stage/monitor/MonitorFROM.java
+++ src/main/java/com/ociweb/pronghorn/stage/monitor/MonitorFROM.java
@@ -1,13 +1,6 @@
 package com.ociweb.pronghorn.stage.monitor;
 
-import java.io.IOException;
-
-import javax.xml.parsers.ParserConfigurationException;
-
-import org.xml.sax.SAXException;
-
 import com.ociweb.pronghorn.ring.FieldReferenceOffsetManager;
-import com.ociweb.pronghorn.ring.schema.loader.TemplateHandler;
 
 public class MonitorFROM {
 	
@@ -19,32 +12,30 @@ public class MonitorFROM {
 	public static final int TEMPLATE_MSG_LOC;
 	public static final int TEMPLATE_SIZE_LOC;
 	
+	public static final FieldReferenceOffsetManager FROM;
+	
 	static {
-		FieldReferenceOffsetManager from = buildFROM();
+				
+		FROM = new FieldReferenceOffsetManager(
+			    new int[]{0xc1400006,0x90800000,0x90800001,0x90800002,0x80000000,0x80200001,0xc1200006,0xc1400008,0x90800000,0x90800001,0x90800002,0x80000000,0x80200001,0x80800002,0x80200003,0xc1200008},
+			    (short)0,
+			    new String[]{"RingStatSample","MS","Head","Tail","TemplateId","BufferSize",null,"RingStatEnhancedSample","MS","Head","Tail","TemplateId","BufferSize","StackDepth","Latency",null},
+			    new long[]{1, 1, 2, 3, 4, 5, 0, 2, 1, 2, 3, 4, 5, 21, 22, 0},
+			    new String[]{"global",null,null,null,null,null,null,"global",null,null,null,null,null,null,null,null},
+			    "ringMonitor.xml");
+				
+		TEMPLATE_LOC = FieldReferenceOffsetManager.lookupTemplateLocator("RingStatSample", FROM);
 		
-		TEMPLATE_LOC = FieldReferenceOffsetManager.lookupTemplateLocator("RingStatSample", from);
+		TEMPLATE_TIME_LOC = FieldReferenceOffsetManager.lookupFieldLocator("MS", TEMPLATE_LOC, FROM);
+		TEMPLATE_HEAD_LOC = FieldReferenceOffsetManager.lookupFieldLocator("Head", TEMPLATE_LOC, FROM);
+		TEMPLATE_TAIL_LOC = FieldReferenceOffsetManager.lookupFieldLocator("Tail", TEMPLATE_LOC, FROM);
+		TEMPLATE_MSG_LOC = FieldReferenceOffsetManager.lookupFieldLocator("TemplateId", TEMPLATE_LOC, FROM);
+		TEMPLATE_SIZE_LOC = FieldReferenceOffsetManager.lookupFieldLocator("BufferSize", TEMPLATE_LOC, FROM);
 		
-		TEMPLATE_TIME_LOC = FieldReferenceOffsetManager.lookupFieldLocator("MS", TEMPLATE_LOC, from);
-		TEMPLATE_HEAD_LOC = FieldReferenceOffsetManager.lookupFieldLocator("Head", TEMPLATE_LOC, from);
-		TEMPLATE_TAIL_LOC = FieldReferenceOffsetManager.lookupFieldLocator("Tail", TEMPLATE_LOC, from);
-		TEMPLATE_MSG_LOC = FieldReferenceOffsetManager.lookupFieldLocator("TemplateId", TEMPLATE_LOC, from);
-		TEMPLATE_SIZE_LOC = FieldReferenceOffsetManager.lookupFieldLocator("BufferSize", TEMPLATE_LOC, from);
 	}
 	
-	/**
-	 * This FROM is provided for easy construction of RingBuffers.
-	 * @return
-	 */
-	public static FieldReferenceOffsetManager buildFROM() {		 
-		try {
-			return TemplateHandler.loadFrom("/ringMonitor.xml");
-		} catch (ParserConfigurationException e) {
-			e.printStackTrace();
-		} catch (SAXException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}		
-		return null;
+	public static FieldReferenceOffsetManager buildFROM() {
+		return FROM;
 	}
+
 }
