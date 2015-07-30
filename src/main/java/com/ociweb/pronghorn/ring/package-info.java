f0ea96e6a98936a056e977d201877083bd72877b
Charles Sharp <cas.nixster@gmail.com>
2015-07-30 15:41:42 -0500

    First pass at the package and overview documentation.

---
+++ src/main/java/com/ociweb/pronghorn/ring/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Contains the classes that implement the Ring's fundamental data structures and provide the low-level access
+ * for reading and writing to the buffers.
+ *
+ * @see <a href="https://github.com/oci-pronghorn/PronghornPipes">Pronghorn Pipes project at GitHub</a>
+ * @author Nathan Tippy (@NathanTippy)
+ * @version 0.0.1-SNAPSHOT
+ */
+package com.ociweb.pronghorn.ring;
