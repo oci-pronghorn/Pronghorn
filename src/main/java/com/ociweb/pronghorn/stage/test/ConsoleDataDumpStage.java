a5bf3790a76adc1a21717098a9126e2baca04258
Nathan Tippy <nathan@tippy.name>
2015-10-25 22:14:14 -0500

    console dump stage to simplify new development

---
+++ src/main/java/com/ociweb/pronghorn/stage/test/ConsoleDataDumpStage.java
@@ -0,0 +1,82 @@
+package com.ociweb.pronghorn.stage.test;
+
+import java.io.PrintStream;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.ociweb.pronghorn.pipe.FieldReferenceOffsetManager;
+import com.ociweb.pronghorn.pipe.Pipe;
+import com.ociweb.pronghorn.pipe.stream.StreamingReadVisitor;
+import com.ociweb.pronghorn.pipe.stream.StreamingReadVisitorToJSON;
+import com.ociweb.pronghorn.pipe.stream.StreamingVisitorReader;
+import com.ociweb.pronghorn.stage.PronghornStage;
+import com.ociweb.pronghorn.stage.scheduling.GraphManager;
+
+public class ConsoleDataDumpStage extends PronghornStage {
+
+    private static Logger log = LoggerFactory.getLogger(ConsoleDataDumpStage.class);
+    
+	private final Pipe input;
+	
+	private StreamingReadVisitor visitor;
+	private StreamingVisitorReader reader;
+	private FieldReferenceOffsetManager from;
+	private final int maxString;
+
+	public ConsoleDataDumpStage(GraphManager graphManager, Pipe input) {
+		super(graphManager, input, NONE);
+		this.input = input;
+		this.maxString = input.maxAvgVarLen;
+	}
+
+	@Override
+	public void startup() {
+		super.startup();		
+		PrintStream out = System.out;
+		
+		try{
+			
+			from = Pipe.from(input);
+            visitor = new StreamingReadVisitorToJSON(out) {
+				@Override
+				public void visitASCII(String name, long id, Appendable value) {
+					assert (((CharSequence)value).length()<maxString) : "Text is too long found "+((CharSequence)value).length();
+					super.visitASCII(name, id, value);
+				}
+				@Override
+				public void visitUTF8(String name, long id, Appendable value) {
+					assert (((CharSequence)value).length()<maxString) : "Text is too long found "+((CharSequence)value).length();
+					super.visitUTF8(name, id, value);
+				}
+			};
+						
+			reader = new StreamingVisitorReader(input, visitor );
+
+			reader.startup();
+								
+		} catch (Throwable t) {
+			throw new RuntimeException(t);
+		}
+	}
+	
+	
+	@Override
+	public void run() {
+		reader.run();
+	}
+	
+
+	@Override
+	public void shutdown() {
+		
+		try{
+			reader.shutdown();
+			
+		} catch (Throwable t) {
+			throw new RuntimeException(t);
+		} 
+	}
+
+
+}
