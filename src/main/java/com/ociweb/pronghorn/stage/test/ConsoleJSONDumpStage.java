a7ba227406e875be5e48613caee9e8bc092a151d
Nathan Tippy <nathan@tippy.name>
2015-10-25 22:25:29 -0500

    simplify class

--- src/main/java/com/ociweb/pronghorn/stage/test/ConsoleJSONDumpStage.java
+++ src/main/java/com/ociweb/pronghorn/stage/test/ConsoleJSONDumpStage.java
@@ -2,10 +2,7 @@ package com.ociweb.pronghorn.stage.test;
 
 import java.io.PrintStream;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import com.ociweb.pronghorn.pipe.FieldReferenceOffsetManager;
+import com.ociweb.pronghorn.pipe.MessageSchema;
 import com.ociweb.pronghorn.pipe.Pipe;
 import com.ociweb.pronghorn.pipe.stream.StreamingReadVisitor;
 import com.ociweb.pronghorn.pipe.stream.StreamingReadVisitorToJSON;
@@ -13,40 +10,33 @@ import com.ociweb.pronghorn.pipe.stream.StreamingVisitorReader;
 import com.ociweb.pronghorn.stage.PronghornStage;
 import com.ociweb.pronghorn.stage.scheduling.GraphManager;
 
-public class ConsoleJSONDumpStage extends PronghornStage {
+public class ConsoleJSONDumpStage<T extends MessageSchema> extends PronghornStage {
 
-    private static Logger log = LoggerFactory.getLogger(ConsoleJSONDumpStage.class);
-    
-	private final Pipe input;
+	private final Pipe<T> input;
 	
 	private StreamingReadVisitor visitor;
 	private StreamingVisitorReader reader;
-	private FieldReferenceOffsetManager from;
-	private final int maxString;
+	private final PrintStream out = System.out;
 
-	public ConsoleJSONDumpStage(GraphManager graphManager, Pipe input) {
+	public ConsoleJSONDumpStage(GraphManager graphManager, Pipe<T> input) {
 		super(graphManager, input, NONE);
 		this.input = input;
-		this.maxString = input.maxAvgVarLen;
 	}
 
 	@Override
 	public void startup() {
 		super.startup();		
-		PrintStream out = System.out;
 		
-		try{
-			
-			from = Pipe.from(input);
+		try{			
             visitor = new StreamingReadVisitorToJSON(out) {
 				@Override
 				public void visitASCII(String name, long id, Appendable value) {
-					assert (((CharSequence)value).length()<maxString) : "Text is too long found "+((CharSequence)value).length();
+					assert (((CharSequence)value).length()<=input.maxAvgVarLen) : "Text is too long found "+((CharSequence)value).length();
 					super.visitASCII(name, id, value);
 				}
 				@Override
 				public void visitUTF8(String name, long id, Appendable value) {
-					assert (((CharSequence)value).length()<maxString) : "Text is too long found "+((CharSequence)value).length();
+					assert (((CharSequence)value).length()<=input.maxAvgVarLen) : "Text is too long found "+((CharSequence)value).length();
 					super.visitUTF8(name, id, value);
 				}
 			};
@@ -58,25 +48,21 @@ public class ConsoleJSONDumpStage extends PronghornStage {
 		} catch (Throwable t) {
 			throw new RuntimeException(t);
 		}
-	}
-	
+	}	
 	
 	@Override
 	public void run() {
 		reader.run();
-	}
-	
+	}	
 
 	@Override
 	public void shutdown() {
 		
 		try{
-			reader.shutdown();
-			
+			reader.shutdown();			
 		} catch (Throwable t) {
 			throw new RuntimeException(t);
 		} 
 	}
-
 
 }
