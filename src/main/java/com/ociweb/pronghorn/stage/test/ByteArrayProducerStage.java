4f9a8b39431c1e56db31dc957265d459cd93f3b6
Nathan Tippy <nathan@tippy.name>
2015-11-01 19:17:57 -0600

    Added constructor for predefined specific chunk sizes.

--- src/main/java/com/ociweb/pronghorn/stage/test/ByteArrayProducerStage.java
+++ src/main/java/com/ociweb/pronghorn/stage/test/ByteArrayProducerStage.java
@@ -9,14 +9,21 @@ public class ByteArrayProducerStage extends PronghornStage{
 
     private final byte[] rawData;
     private final int rawDataLength;
+    private final int[] optionalChunkSizes;
+    private int chunkCount;
     private int pos;
     private final int chunkSize;
     private final Pipe<RawDataSchema> output;
     
     public ByteArrayProducerStage(GraphManager gm, byte[] rawData, Pipe<RawDataSchema> output) {
+        this(gm, rawData, null, output);
+    }
+    
+    public ByteArrayProducerStage(GraphManager gm, byte[] rawData, int[] optionalChunkSizes, Pipe<RawDataSchema> output) {
         super(gm, NONE, output);
         this.rawData = rawData;
         this.rawDataLength = rawData.length;
+        this.optionalChunkSizes = optionalChunkSizes;
         this.pos = 0;
         this.chunkSize =output.maxAvgVarLen;
         this.output = output;
@@ -28,7 +35,8 @@ public class ByteArrayProducerStage extends PronghornStage{
         
         while (pos<rawDataLength && Pipe.hasRoomForWrite(output)) {
                         
-            int length = Math.min(chunkSize, rawDataLength-pos);
+            int length = computeLength();
+                        
             assert(length>0);
             
             int size = Pipe.addMsgIdx(output, 0);
@@ -37,13 +45,26 @@ public class ByteArrayProducerStage extends PronghornStage{
             Pipe.confirmLowLevelWrite(output, size);
             
             pos+=length;
-            
+            ++chunkCount;
         }
         
         if (pos==rawData.length) {
             Pipe.publishAllBatchedWrites(output);
             requestShutdown();
         }
+    }
+
+    private int computeLength() {
+        int length = Math.min(chunkSize, rawDataLength-pos);
+        
+        if (null != optionalChunkSizes) {
+            int defLen = optionalChunkSizes[chunkCount];
+            if (defLen>length) {
+                throw new UnsupportedOperationException("defined chunk length must have data and fit within the pipe");
+            }
+            length=defLen;
+        }
+        return length;
     }
     
    
