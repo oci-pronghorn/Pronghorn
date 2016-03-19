60ae7f7d19c49b25a5c3be7877eab7ffe529df68
Jake <jakking@siue.edu>
2016-03-19 17:34:46 -0500

    Fixed unit test, the method passed

--- src/test/java/com/ociweb/pronghorn/stage/phast/PhastEncoderTest.java
+++ src/test/java/com/ociweb/pronghorn/stage/phast/PhastEncoderTest.java
@@ -1,4 +1,5 @@
 package com.ociweb.pronghorn.stage.phast;
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 
 import org.junit.Test;
@@ -16,12 +17,28 @@ import static org.junit.Assert.*;
 public class PhastEncoderTest {
 	
 	@Test
-	public void testEncodeString() throws UnsupportedEncodingException{
-		//PipeConfig<RawDataSchema> inputConfig = new PipeConfig<RawDataSchema>(RawDataSchema.instance, 100, 1000);
-		//Pipe<RawDataSchema> testDataToDecode = new Pipe<RawDataSchema>(inputConfig);
-		//DataOutputBlobWriter<RawDataSchema> writer = new DataOutputBlobWriter<RawDataSchema>(testDataToDecode);
+	public void testEncodeString() throws IOException{
+		//create a new blob pipe to put a string on 
+		Pipe<RawDataSchema> encodedValuesToValidate = new Pipe<RawDataSchema>(new PipeConfig<RawDataSchema>(RawDataSchema.instance, 100, 4000));
+		encodedValuesToValidate.initBuffers();
+		DataOutputBlobWriter<RawDataSchema> writer = new DataOutputBlobWriter<RawDataSchema>(encodedValuesToValidate);
 		
-		//PhastEncoder.encodeString(writer, 0, 0, "this is a test");
-		assertTrue(true);
+		//encode a string on there using the static method
+		PhastEncoder.encodeString(writer, 0, 0, "This is a test");
+		Pipe.publishAllBatchedWrites(encodedValuesToValidate);
+		
+		//check what is on the pipe
+		DataInputBlobReader<RawDataSchema> reader = new DataInputBlobReader<RawDataSchema>(encodedValuesToValidate);
+		//should be -63
+		int test = reader.readPackedInt();
+		//char length is 14 so this should be 28
+		int lengthOfString = reader.readPackedInt();
+		//the string
+		StringBuilder value = new StringBuilder();
+		reader.readPackedChars(value);
+		
+		String s = value.toString();
+		assertTrue((test==-63) && (lengthOfString==28) && (s.compareTo("This is a test")==0));
+		
 	}
 }
