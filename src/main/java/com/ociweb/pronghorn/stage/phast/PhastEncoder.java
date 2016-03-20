c58ad64de10979ecc0a91fba6bf421daddffb8c0
Jake <jakking@siue.edu>
2016-03-20 18:34:57 -0500

    finished everything with Ints, working on longs, and made two more tests

--- src/main/java/com/ociweb/pronghorn/stage/phast/PhastEncoder.java
+++ src/main/java/com/ociweb/pronghorn/stage/phast/PhastEncoder.java
@@ -1,5 +1,6 @@
 package com.ociweb.pronghorn.stage.phast;
 
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 
 import com.ociweb.pronghorn.pipe.DataOutputBlobWriter;
@@ -13,11 +14,14 @@ public class PhastEncoder {
         }
     }
 
-    //i think this was backwards applied fix
+    //i think this was backwards, applied fix
     static void encodeDeltaInt(int[] intDictionary, DataOutputBlobWriter writer, long pmapHeader, int bitMask, int idx, int value) {
         if (0 != (pmapHeader&bitMask)) {
             DataOutputBlobWriter.writePackedInt(writer, value-intDictionary[idx]);
             intDictionary[idx] = value;
+        }
+        else{
+        	DataOutputBlobWriter.writePackedInt(writer, intDictionary[idx]);
         }
     }
 
@@ -33,7 +37,7 @@ public class PhastEncoder {
     //this method encodes a string
     static void encodeString(DataOutputBlobWriter writer, String value) throws UnsupportedEncodingException{
     	//encode -63 so it knows it is variable length
-    	DataOutputBlobWriter.writePackedUInt(writer, -63);
+    	DataOutputBlobWriter.writePackedInt(writer, -63);
     		 
     	//calculate string length in bytes, then encode it
     	byte[] byteArray = value.getBytes("UTF-16BE");
@@ -53,6 +57,48 @@ public class PhastEncoder {
     static void copyInt(int[] intDictionary, DataOutputBlobWriter writer, long pmapHeader, int bitMask, int idx){
     	if (0 == (pmapHeader&bitMask)) {
     		DataOutputBlobWriter.writePackedInt(writer, intDictionary[idx]);
+    	}
+    }
+    
+    //encodes the default value from the default value dictionary
+    static void encodeDefaultInt(int[] defaultIntDictionary, DataOutputBlobWriter writer, long pmapHeader, int bitmask, int idx, int value){
+    	if (0 == (pmapHeader & bitmask)){
+    		DataOutputBlobWriter.writePackedInt(writer, defaultIntDictionary[idx]);
+    	}
+    	else{
+    		DataOutputBlobWriter.writePackedInt(writer, value);
+    	}
+    }
+    
+    //encodes long that is present in the pmap
+    static void encodeLongPresent(DataOutputBlobWriter writer, long pmapHeader, int bitMask, long value) {
+        if (0 != (pmapHeader&bitMask)) {
+        	DataOutputBlobWriter.writePackedLong(writer, value);
+        }
+    }
+    
+    //this method increments a dictionary value by one, then writes it to the pipe
+    static void incrementLong(long[] longDictionary, DataOutputBlobWriter writer, long pmapHeader, int bitMask, int idx){
+    	if (0 != (pmapHeader&bitMask)) {
+    		longDictionary[idx]++;
+    	}
+    		DataOutputBlobWriter.writePackedLong(writer, longDictionary[idx]);
+    }
+    
+    //this method just uses the previous value that was sent
+    static void copyLong(long[] longDictionary, DataOutputBlobWriter writer, long pmapHeader, int bitMask, int idx){
+    	if (0 == (pmapHeader&bitMask)) {
+    		DataOutputBlobWriter.writePackedLong(writer, longDictionary[idx]);
+    	}
+    }
+    
+    //encodes default value for a long
+    static void encodeDefaultLong(long[] defaultLongDictionary, DataOutputBlobWriter writer, long pmapHeader, int bitmask, int idx, long value){
+    	if (0 != (pmapHeader & bitmask)){
+    		DataOutputBlobWriter.writePackedLong(writer, defaultLongDictionary[idx]);
+    	}
+    	else{
+    		DataOutputBlobWriter.writePackedLong(writer, value);
     	}
     }
     
--- src/test/java/com/ociweb/pronghorn/stage/phast/PhastEncoderTest.java
+++ src/test/java/com/ociweb/pronghorn/stage/phast/PhastEncoderTest.java
@@ -27,6 +27,7 @@ public class PhastEncoderTest {
 		//encode a string on there using the static method
 		PhastEncoder.encodeString(writer, "This is a test");
 		Pipe.publishAllBatchedWrites(encodedValuesToValidate);
+		writer.close();
 		
 		//check what is on the pipe
 		DataInputBlobReader<RawDataSchema> reader = new DataInputBlobReader<RawDataSchema>(encodedValuesToValidate);
@@ -46,7 +47,7 @@ public class PhastEncoderTest {
 	}
 	
 	@Test
-	public void testIncrementInt(){
+	public void testIncrementInt() throws IOException{
 		//create blob for test
 		Pipe<RawDataSchema> encodedValuesToValidate = new Pipe<RawDataSchema>(new PipeConfig<RawDataSchema>(RawDataSchema.instance, 100, 4000));
 		encodedValuesToValidate.initBuffers();
@@ -58,37 +59,84 @@ public class PhastEncoderTest {
 		intDictionary[2] = 5;
 		intDictionary[1] = 5;
 		
-		//make it increment 2 values 0 and 5
+		//increment one and not the other
 		PhastEncoder.incrementInt(intDictionary, writer, 0, 1, 2);
 		PhastEncoder.incrementInt(intDictionary, writer, 1, 1, 1);
+		
+		writer.close();
 		
 		DataInputBlobReader<RawDataSchema> reader = new DataInputBlobReader<RawDataSchema>(encodedValuesToValidate);
 		//should be 5
 		int test1 = reader.readPackedInt();
 		//should be 6
 		int test2 = reader.readPackedInt();
+		reader.close();
 		
 		assertTrue((test1 == 5) && (test2==6));
 	}
 	
 	@Test
-	public void copyIntTest(){
+	public void copyIntTest() throws IOException{
 		//create blob for test
-			Pipe<RawDataSchema> encodedValuesToValidate = new Pipe<RawDataSchema>(new PipeConfig<RawDataSchema>(RawDataSchema.instance, 100, 4000));
-			encodedValuesToValidate.initBuffers();
-			DataOutputBlobWriter<RawDataSchema> writer = new DataOutputBlobWriter<RawDataSchema>(encodedValuesToValidate);
-			
-			//create int dictionary
-			int[] intDictionary = new int[5];
-			Arrays.fill(intDictionary, 0);
-			intDictionary[2] = 5;
-			
-			//make it increment 2 values 0 and 5
-			PhastEncoder.copyInt(intDictionary, writer, 0, 0, 2);
-			
-			DataInputBlobReader<RawDataSchema> reader = new DataInputBlobReader<RawDataSchema>(encodedValuesToValidate);
-			//should be 5
-			int test1 = reader.readPackedInt();
-			assertTrue(test1 == 5);
+		Pipe<RawDataSchema> encodedValuesToValidate = new Pipe<RawDataSchema>(new PipeConfig<RawDataSchema>(RawDataSchema.instance, 100, 4000));
+		encodedValuesToValidate.initBuffers();
+		DataOutputBlobWriter<RawDataSchema> writer = new DataOutputBlobWriter<RawDataSchema>(encodedValuesToValidate);
+		
+		//create int dictionary
+		int[] intDictionary = new int[5];
+		Arrays.fill(intDictionary, 0);
+		intDictionary[2] = 5;
+		
+		//make it increment 2 values 0 and 5
+		PhastEncoder.copyInt(intDictionary, writer, 0, 0, 2);
+		writer.close();
+		
+		DataInputBlobReader<RawDataSchema> reader = new DataInputBlobReader<RawDataSchema>(encodedValuesToValidate);
+		//should be 5
+		int test1 = reader.readPackedInt();
+		reader.close();
+		assertTrue(test1 == 5);
+	}
+	
+	@Test
+	public void defaultIntTest() throws IOException{
+		//create a blob to test
+		Pipe<RawDataSchema> encodedValuesToValidate = new Pipe<RawDataSchema>(new PipeConfig<RawDataSchema>(RawDataSchema.instance, 100, 4000));
+		encodedValuesToValidate.initBuffers();
+		DataOutputBlobWriter<RawDataSchema> writer = new DataOutputBlobWriter<RawDataSchema>(encodedValuesToValidate);
+		
+		//make int array
+		int[] defaultInt = new int[5];
+		defaultInt[3] = 4;
+		
+		//should encode 16
+		PhastEncoder.encodeDefaultInt(defaultInt, writer, 1, 1, 3, 16);
+		//should encode 4
+		PhastEncoder.encodeDefaultInt(defaultInt, writer, 0, 1, 3, 16);
+		
+		writer.close();
+		DataInputBlobReader<RawDataSchema> reader = new DataInputBlobReader<RawDataSchema>(encodedValuesToValidate);
+		int test1 = reader.readPackedInt();
+		int test2 = reader.readPackedInt();
+		reader.close();
+		
+		assertTrue(test1==16 && test2==4);
+	}
+	
+	@Test
+	public void ecnodePresentLongTest() throws IOException{
+		//create a blob to test
+		Pipe<RawDataSchema> encodedValuesToValidate = new Pipe<RawDataSchema>(new PipeConfig<RawDataSchema>(RawDataSchema.instance, 100, 4000));
+		encodedValuesToValidate.initBuffers();
+		DataOutputBlobWriter<RawDataSchema> writer = new DataOutputBlobWriter<RawDataSchema>(encodedValuesToValidate);
+		
+		//encode long
+		PhastEncoder.encodeLongPresent(writer, 1, 1, 5714);
+		writer.close();
+		
+		//check it
+		DataInputBlobReader<RawDataSchema> reader = new DataInputBlobReader<RawDataSchema>(encodedValuesToValidate);
+		long test = reader.readPackedLong();
+		assertTrue(test == 5714);
 	}
 }
