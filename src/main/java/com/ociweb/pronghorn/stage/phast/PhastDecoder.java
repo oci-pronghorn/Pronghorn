2288fb00795649f9787657bcd4e934ba565e42b0
Jake <jakking@siue.edu>
2016-03-27 20:29:57 -0500

    finished decoding ints, and unit tests to go with

--- src/main/java/com/ociweb/pronghorn/stage/phast/PhastDecoder.java
+++ src/main/java/com/ociweb/pronghorn/stage/phast/PhastDecoder.java
@@ -1,5 +1,7 @@
 package com.ociweb.pronghorn.stage.phast;
 
+import java.io.IOException;
+
 import com.ociweb.pronghorn.pipe.DataInputBlobReader;
 
 public class PhastDecoder {
@@ -8,12 +10,12 @@ public class PhastDecoder {
         return (0==(map&bitMask)) ? (longDictionary[idx] += DataInputBlobReader.readPackedLong(reader)) : defaultValue;        
     }
 
-    static int decodeDefaultInt(DataInputBlobReader reader, long map, int defaultValue, int bitMask) {
-       return (0==(map&bitMask)) ? defaultValue : DataInputBlobReader.readPackedInt(reader);
+    static int decodeDefaultInt(DataInputBlobReader reader, long map, int[] defaultValues, int bitMask, int idx) {
+       return (0==(map&bitMask)) ? defaultValues[idx] : DataInputBlobReader.readPackedInt(reader);
     }
 
-    static int decodeDeltaInt(int[] intDictionary, DataInputBlobReader reader, long map, int idx, int defaultValue, int bitMask) {
-        return (0==(map&bitMask)) ? (intDictionary[idx] += DataInputBlobReader.readPackedInt(reader)) : defaultValue;
+    static int decodeDeltaInt(int[] intDictionary, DataInputBlobReader reader, long map, int idx, int bitMask, int value) {
+        return (0==(map&bitMask)) ? (intDictionary[idx] += DataInputBlobReader.readPackedInt(reader)) : intDictionary[idx];
     }
 
     static int decodeIncInt(int[] intDictionary, DataInputBlobReader reader, long map, int idx, int bitMask) {
@@ -33,11 +35,24 @@ public class PhastDecoder {
     
     //decodes an increment int
     static int decodeIncrementInt(int[] intDictionary, long map, int idx, int bitMask){
-    	return (0==(map&bitMask))? intDictionary[idx]++ : intDictionary[idx];
+    	return (0==(map&bitMask))? ++intDictionary[idx] : intDictionary[idx];
     }
     
     //decodes present int
     static int decodePresentInt(DataInputBlobReader reader, long map, int bitMask){
     	return(0==(map&bitMask))? DataInputBlobReader.readPackedInt(reader) : null;
+    }
+    
+    //decodes string
+    static String decodeString(DataInputBlobReader slab, DataInputBlobReader blob) throws IOException{
+    	if (DataInputBlobReader.readPackedInt(slab) == -63){
+    		StringBuilder s = new StringBuilder();
+    		DataInputBlobReader.readPackedChars(blob, s);
+    		return s.toString();
+    	}
+    	else{
+    		return "FATALITY";
+    	}
+    	
     }
 }
--- src/test/java/com/ociweb/pronghorn/stage/phast/PhastDecoderTest.java
+++ src/test/java/com/ociweb/pronghorn/stage/phast/PhastDecoderTest.java
@@ -16,34 +16,84 @@ import com.ociweb.pronghorn.stage.phast.*;
 
 
 public class PhastDecoderTest {
+	//string test
+	@Test
+	public void decodeStringTest() throws IOException{
+		Pipe<RawDataSchema> slab = new Pipe<RawDataSchema>(new PipeConfig<RawDataSchema>(RawDataSchema.instance, 100, 4000));
+		Pipe<RawDataSchema> blob = new Pipe<RawDataSchema>(new PipeConfig<RawDataSchema>(RawDataSchema.instance, 100, 4000));
+		slab.initBuffers();
+		blob.initBuffers();
+		DataOutputBlobWriter<RawDataSchema> writerSlab = new DataOutputBlobWriter<RawDataSchema>(slab);
+		DataOutputBlobWriter<RawDataSchema> writerBlob = new DataOutputBlobWriter<RawDataSchema>(blob);
+		
+		DataOutputBlobWriter.writePackedInt(writerSlab, -63);
+		DataOutputBlobWriter.writePackedInt(writerSlab, 28);
+		DataOutputBlobWriter.writePackedChars(writerBlob, "This is a test");
+		
+		writerSlab.close();
+		writerBlob.close();
+		
+		DataInputBlobReader<RawDataSchema> readerSlab = new DataInputBlobReader<RawDataSchema>(slab);
+		DataInputBlobReader<RawDataSchema> readerBlob = new DataInputBlobReader<RawDataSchema>(blob);
+		
+		String stest = PhastDecoder.decodeString(readerSlab, readerBlob);
+		
+		assertTrue(stest.compareTo("This is a test") == 0);
+		
+	}
 
 	/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	////////////////////////                    MASTER INT DECODE TEST INCLUDES ALL INT TESTS                    ////////////////////////////
 	/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	@Test
-	static void decodeIntTests() throws IOException{
+	public void decodeIntTests() throws IOException{
 		Pipe<RawDataSchema> slab = new Pipe<RawDataSchema>(new PipeConfig<RawDataSchema>(RawDataSchema.instance, 100, 4000));
 		slab.initBuffers();
 		DataOutputBlobWriter<RawDataSchema> writerSlab = new DataOutputBlobWriter<RawDataSchema>(slab);
 		
 		//writing testing ints
 		DataOutputBlobWriter.writePackedInt(writerSlab, 3894);
-		DataOutputBlobWriter.writePackedInt(writerSlab, 34893);
 		DataOutputBlobWriter.writePackedInt(writerSlab, 905);
+		DataOutputBlobWriter.writePackedInt(writerSlab, 903);
 		DataOutputBlobWriter.writePackedInt(writerSlab, 404);
-		DataOutputBlobWriter.writePackedInt(writerSlab, 493);
-		DataOutputBlobWriter.writePackedInt(writerSlab, 9403);
-		DataOutputBlobWriter.writePackedInt(writerSlab, 84);
-		DataOutputBlobWriter.writePackedInt(writerSlab, 3980);
-		DataOutputBlobWriter.writePackedInt(writerSlab, 202);
-		DataOutputBlobWriter.writePackedInt(writerSlab, 4893);
-		DataOutputBlobWriter.writePackedInt(writerSlab, 2034);
 		
 		writerSlab.close();
 		
-		assertTrue(true);
+		int[] intDictionary = new int[5];
+		intDictionary[0] = 3;
+		intDictionary[1] = 56;
+		intDictionary[2] = 70;
+		
+		int[] defaultValues = new int[5];
+		defaultValues[0] = 5;
+		defaultValues[1] = 60;
+		defaultValues[2] = 16;
 		
 		
+		//present int test
+		DataInputBlobReader<RawDataSchema> readerSlab = new DataInputBlobReader<RawDataSchema>(slab);
+		int test = PhastDecoder.decodePresentInt(readerSlab, 0, 0);
+		assertTrue(test==3894);
+		
+		//delta int test
+		test = PhastDecoder.decodeDeltaInt(intDictionary, readerSlab, 0, 1, 0, 10);
+		int test2 = PhastDecoder.decodeDeltaInt(intDictionary, readerSlab, 1, 1, 1, 0);
+		assertTrue(test==961 && test2==961);
+		
+		//increment test
+		test = PhastDecoder.decodeIncrementInt(intDictionary, 0, 0, 0);
+		test2 = PhastDecoder.decodeIncrementInt(intDictionary, 1, 0, 1);
+		assertTrue(test == 4 && test2 == 4);
+		
+		//copy int test
+		test = PhastDecoder.decodeCopyInt(intDictionary, readerSlab, 0, 2, 0);
+		test2 = PhastDecoder.decodeCopyInt(intDictionary, readerSlab, 1, 2, 1);
+		assertTrue(test == 70 && test2 == 903);
+		
+		//default int test
+		test = PhastDecoder.decodeDefaultInt(readerSlab, 0, defaultValues, 0, 2);
+		test2 = PhastDecoder.decodeDefaultInt(readerSlab, 1, defaultValues, 1, 2);
+		assertTrue(test == 16 && test2==404);
 		
 	}
 }
