e2f791b9815775e90114cdfe7eb9ab2f7aa77fc6
Jake <jakking@siue.edu>
2016-03-31 20:55:46 -0500

    Unit tests and methods for longs

--- src/test/java/com/ociweb/pronghorn/stage/phast/PhastDecoderTest.java
+++ src/test/java/com/ociweb/pronghorn/stage/phast/PhastDecoderTest.java
@@ -96,4 +96,59 @@ public class PhastDecoderTest {
 		assertTrue(test == 16 && test2==404);
 		
 	}
+	
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////                    MASTER LONG DECODE TEST INCLUDES ALL LONG TESTS                  ////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	@Test
+	public void decodeLongTests() throws IOException{
+		Pipe<RawDataSchema> slab = new Pipe<RawDataSchema>(new PipeConfig<RawDataSchema>(RawDataSchema.instance, 100, 4000));
+		slab.initBuffers();
+		DataOutputBlobWriter<RawDataSchema> writerSlab = new DataOutputBlobWriter<RawDataSchema>(slab);
+		
+		//writing testing longs
+		DataOutputBlobWriter.writePackedLong(writerSlab, 3894);
+		DataOutputBlobWriter.writePackedLong(writerSlab, 905);
+		DataOutputBlobWriter.writePackedLong(writerSlab, 903);
+		DataOutputBlobWriter.writePackedLong(writerSlab, 404);
+		
+		writerSlab.close();
+		
+		long[] longDictionary = new long[5];
+		longDictionary[0] = 3;
+		longDictionary[1] = 56;
+		longDictionary[2] = 70;
+		
+		long[] defaultValues = new long[5];
+		defaultValues[0] = 5;
+		defaultValues[1] = 60;
+		defaultValues[2] = 16;
+		
+		
+		//present long test
+		DataInputBlobReader<RawDataSchema> readerSlab = new DataInputBlobReader<RawDataSchema>(slab);
+		long test = PhastDecoder.decodePresentLong(readerSlab, 0, 0);
+		assertTrue(test==3894);
+		
+		//delta long test
+		test = PhastDecoder.decodeDeltaLong(longDictionary, readerSlab, 0, 1, 0);
+		long test2 = PhastDecoder.decodeDeltaLong(longDictionary, readerSlab, 1, 1, 1);
+		assertTrue(test==961 && test2==961);
+		
+		//increment test
+		test = PhastDecoder.decodeIncrementLong(longDictionary, 0, 0, 0);
+		test2 = PhastDecoder.decodeIncrementLong(longDictionary, 1, 0, 1);
+		assertTrue(test == 4 && test2 == 4);
+		
+		//copy long test
+		test = PhastDecoder.decodeCopyLong(longDictionary, readerSlab, 0, 2, 0);
+		test2 = PhastDecoder.decodeCopyLong(longDictionary, readerSlab, 1, 2, 1);
+		assertTrue(test == 70 && test2 == 903);
+		
+		//default long test
+		test = PhastDecoder.decodeDefaultLong(readerSlab, 0, defaultValues, 0, 2);
+		test2 = PhastDecoder.decodeDefaultLong(readerSlab, 1, defaultValues, 1, 2);
+		assertTrue(test == 16 && test2==404);
+		
+	}
 }
