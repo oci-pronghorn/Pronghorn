780c4dffb073dd8e7ca140689e38219fa11f78bb
Nathan Tippy <nathan@tippy.name>
2016-07-31 21:58:59 -0500

    added to string

--- src/main/java/com/ociweb/pronghorn/util/math/ScriptedSchedule.java
+++ src/main/java/com/ociweb/pronghorn/util/math/ScriptedSchedule.java
@@ -1,5 +1,9 @@
 package com.ociweb.pronghorn.util.math;
 
+import java.io.IOException;
+
+import com.ociweb.pronghorn.util.Appendables;
+
 public class ScriptedSchedule {
 
     //package protected
@@ -10,6 +14,22 @@ public class ScriptedSchedule {
     public ScriptedSchedule(int commonClock, byte[] script) {
         this.commonClock = commonClock;
         this.script = script;
+    }
+    
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        
+        try {
+        
+            Appendables.appendValue(builder, "Clock:", commonClock, "ms  Script:");
+            Appendables.appendArray(builder, '[', script, ']');
+            
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+                
+        return builder.toString();
     }
     
 
