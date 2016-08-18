f3074b678a48a8cfe475ed2531ef2e2498fe1ce6
jsarnie <jsarnie@gmail.com>
2016-08-18 18:15:11 -0500

    Getter Setter Test Format

--- src/main/java/com/ociweb/pronghorn/stage/generator/protoBufInterface/ProtoBuffInterface.java
+++ src/main/java/com/ociweb/pronghorn/stage/generator/protoBufInterface/ProtoBuffInterface.java
@@ -8,6 +8,7 @@ package com.ociweb.pronghorn.stage.generator.protoBufInterface;
 import com.ociweb.pronghorn.pipe.MessageSchema;
 import com.ociweb.pronghorn.stage.generator.PhastDecoderStageGenerator;
 import com.ociweb.pronghorn.stage.generator.PhastEncoderStageGenerator;
+import java.io.IOException;
 
 /**
  *
@@ -29,6 +30,58 @@ public class ProtoBuffInterface {
         this.schema = schema;
         this.interfaceTarget = interfaceTarget;
         this.interfaceClassName = interfaceClassName;
+    }
+    // Do i need this if we are bringing int he encoderGenerator already?
+    /*@Override
+    protected void additionalTokens(Appendable target) throws IOException {
+        FieldReferenceOffsetManager from = MessageSchema.from(schema);
+        int[] tokens = from.tokens;
+        String[] scriptNames = from.fieldNameScript;
+        long[] scriptIds = from.fieldIdScript;
+        int i = tokens.length;
+
+        while (--i >= 0) {
+            int type = TokenBuilder.extractType(tokens[i]);
+
+            if (TypeMask.isLong(type)) {
+                target.append("private long ").append(scriptNames[i]).append(";\n");
+            } else if (TypeMask.isInt(type)) {
+                target.append("private int ").append(scriptNames[i]).append(";\n");
+            } else if (TypeMask.isText(type)) {
+                target.append("private String ").append(scriptNames[i]).append(";\n");
+            }
+        }
+    }
+    */
+    //public String getSalutation() { return salutation; }
+    protected void InterfaceBuilderGetterString(MessageSchema schema, Appendable interfaceTarget, int idx, String stringName) {
+        try {
+            appendStaticCall(interfaceTarget, encoderGenerator, "InterfaceBuilderGetter")
+                    .append("public String get")
+                    .append(stringName).append("() { return ")
+                    .append(stringName).append("; }")
+                    .append(");\n");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    //public void setName(String name) { this.name = name; }
+    protected void InterfaceBuilderSetterString(MessageSchema schema, Appendable interfaceTarget, int idx, String stringName) {
+        try {
+            appendStaticCall(interfaceTarget, encoderGenerator, "InterfaceBuilderSetter")
+                    .append("public void set")
+                    .append(stringName).append("(String ")
+                    .append(stringName).append(") { this.")
+                    .append(stringName).append(" = ")
+                    .append(stringName).append("; }")
+                    .append(");\n");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    
+    public class interfaceClassName {
+        
     }
     
 }
