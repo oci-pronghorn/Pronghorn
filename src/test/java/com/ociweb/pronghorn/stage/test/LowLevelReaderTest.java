19dfbbdf31ea16d7fb0877b72ef809d1e586f54e
amattis <amatt@Alex-PC>
2016-09-27 14:03:07 -0500

    Added LowLevelWriter and LowLevelReader Test Currently fails

---
+++ src/test/java/com/ociweb/pronghorn/stage/test/LowLevelReaderTest.java
@@ -0,0 +1,69 @@
+package com.ociweb.pronghorn.stage.test;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
+
+import java.lang.reflect.Constructor;
+import org.junit.Test;
+
+import com.ociweb.pronghorn.code.LoaderUtil;
+import com.ociweb.pronghorn.pipe.Pipe;
+import com.ociweb.pronghorn.stage.PronghornStage;
+import com.ociweb.pronghorn.stage.generator.FuzzDataStageGenerator;
+import com.ociweb.pronghorn.stage.scheduling.GraphManager;
+import com.ociweb.pronghorn.pipe.build.LowLevelReader;
+
+/**
+ *
+ * @author amatt
+ */
+public class LowLevelReaderTest {
+    
+    @Test
+        public void lowLevelReaderStartupTest(){
+            StringBuilder target = new StringBuilder();
+            LowLevelReader rw = new LowLevelReader();
+            
+            rw.startup();
+           
+        }
+        
+        @Test
+        public void lowLevelReaderRunTest() {
+            //StringBuilder target = new StringBuilder();
+            LowLevelReader rw = new LowLevelReader();
+            
+            rw.run();
+            
+            //validateCleanCompile();
+            
+        }
+        
+        
+        private static void validateCleanCompile(String packageName, String className, StringBuilder target) {
+        try {
+
+            Class generateClass = LoaderUtil.generateClass(packageName, className, target, FuzzDataStageGenerator.class);
+            
+            if (generateClass.isAssignableFrom(PronghornStage.class)) {
+                Constructor constructor =  generateClass.getConstructor(GraphManager.class, Pipe.class);
+                assertNotNull(constructor);
+            }
+        
+        } catch (ClassNotFoundException e) {
+            System.out.println(target);
+            e.printStackTrace();
+            fail();
+        } catch (NoSuchMethodException e) {
+            System.out.println(target);
+            e.printStackTrace();
+            fail();
+        } catch (SecurityException e) {
+            System.out.println(target);
+            e.printStackTrace();
+            fail();
+        }
+        
+    }
+    
+}
---
+++ src/test/java/com/ociweb/pronghorn/stage/test/LowLevelWriterTest.java
@@ -0,0 +1,70 @@
+package com.ociweb.pronghorn.stage.test;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
+
+import java.io.IOException;
+import java.lang.reflect.Constructor;
+import org.junit.Test;
+
+import com.ociweb.pronghorn.code.LoaderUtil;
+import com.ociweb.pronghorn.pipe.DataOutputBlobWriter;
+import com.ociweb.pronghorn.pipe.Pipe;
+import com.ociweb.pronghorn.pipe.PipeConfig;
+import com.ociweb.pronghorn.pipe.RawDataSchema;
+import com.ociweb.pronghorn.stage.PronghornStage;
+import com.ociweb.pronghorn.stage.generator.FuzzDataStageGenerator;
+import com.ociweb.pronghorn.stage.scheduling.GraphManager;
+import com.ociweb.pronghorn.pipe.build.LowLevelWriter;
+
+/**
+ *
+ * @author amatt
+ */
+public class LowLevelWriterTest{
+        
+    
+        @Test
+        public void lowLevelWriterStartupTest(){            
+            LowLevelWriter lw = new LowLevelWriter();
+            
+            lw.startup();
+        }
+        
+        @Test
+        public void lowLevelWriterRunTest() {
+            //StringBuilder target = new StringBuilder();
+            LowLevelWriter lw = new LowLevelWriter();
+            
+            lw.run();            
+        }
+        
+        
+        private static void validateCleanCompile(String packageName, String className, StringBuilder target) {
+        try {
+
+            Class generateClass = LoaderUtil.generateClass(packageName, className, target, FuzzDataStageGenerator.class);
+            
+            if (generateClass.isAssignableFrom(PronghornStage.class)) {
+                Constructor constructor =  generateClass.getConstructor(GraphManager.class, Pipe.class);
+                assertNotNull(constructor);
+            }
+        
+        } catch (ClassNotFoundException e) {
+            System.out.println(target);
+            e.printStackTrace();
+            fail();
+        } catch (NoSuchMethodException e) {
+            System.out.println(target);
+            e.printStackTrace();
+            fail();
+        } catch (SecurityException e) {
+            System.out.println(target);
+            e.printStackTrace();
+            fail();
+        }
+        
+    }
+        
+    
+}
