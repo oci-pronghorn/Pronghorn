e7e3f9d6b131d05e03645d8e6c360891688c6f72
Jake King <jakking@siue.edu>
2016-10-18 16:17:37 -0500

    fixed null pointer

--- src/test/java/com/ociweb/pronghorn/stage/RandomWriterStage.java
+++ src/test/java/com/ociweb/pronghorn/stage/RandomWriterStage.java
@@ -14,46 +14,62 @@ public class RandomWriterStage extends PronghornStage{
 
 
     Pipe<MessageSchemaDynamic>  output;
-
+    private int i;
+    private Random rnd;
 
     public RandomWriterStage(GraphManager gm, Pipe<MessageSchemaDynamic>  output){
         super(gm,NONE,output);
         this.output = output;
+        i=0;
+    }
+
+    @Override
+    public void startup(){
+        rnd = new Random();
     }
 
     @Override
     public void run() {
-            Random rnd = new Random();
-            int random, storeID, amount, recordID;
-            long date;
-            String productName, units;
-            for (int i = 0; i < 10; i++){
 
-                if (Pipe.hasRoomForWrite(output)) {
-                    //generate random numbers
-                    random = rnd.nextInt(50000);
-                    storeID = rnd.nextInt(50000);
-                    date = (long) rnd.nextInt(50000);
-                    productName = "first string test " + Integer.toString(rnd.nextInt(50000));
-                    amount = random * 100;
-                    recordID = i;
-                    units = "second string test " + Integer.toString(rnd.nextInt(50000));
-                    //put message
-                    Pipe.addMsgIdx(output, 0);
-                    //place them on the pipe
-                    Pipe.addIntValue(storeID, output);
-                    Pipe.addLongValue(date, output);
-                    Pipe.addASCII(productName, output);
-                    Pipe.addIntValue(amount, output);
-                    Pipe.addIntValue(recordID, output);
-                    Pipe.addASCII(units, output);
-                    Pipe.confirmLowLevelWrite(output, 11);
-                    Pipe.publishWrites(output);
-                }
+        while (Pipe.hasRoomForWrite(output)) {
+            if (++i<10){
+                int random, storeID, amount, recordID;
+                long date;
+                String productName, units;
+
+                random = rnd.nextInt(50000);
+                storeID = rnd.nextInt(50000);
+                date = (long) rnd.nextInt(50000);
+                productName = "first string test " + Integer.toString(rnd.nextInt(50000));
+                amount = rnd.nextInt(50000);
+                recordID = i;
+                units = "second string test " + Integer.toString(rnd.nextInt(50000));
+
+                //generate random numbers
+                random = rnd.nextInt(50000);
+                storeID = rnd.nextInt(50000);
+                date = (long) rnd.nextInt(50000);
+                productName = "first string test " + Integer.toString(rnd.nextInt(50000));
+                amount = random * 100;
+                recordID = i;
+                units = "second string test " + Integer.toString(rnd.nextInt(50000));
+                //put message
+                Pipe.addMsgIdx(output, 0);
+                //place them on the pipe
+                Pipe.addIntValue(storeID, output);
+                Pipe.addLongValue(date, output);
+                Pipe.addASCII(productName, output);
+                Pipe.addIntValue(amount, output);
+                Pipe.addIntValue(recordID, output);
+                Pipe.addASCII(units, output);
+                Pipe.confirmLowLevelWrite(output, 11);
+                Pipe.publishWrites(output);
             }
-
-            Pipe.publishEOF(output);
-
+            else {
+                Pipe.publishEOF(output);
+                requestShutdown();
+            }
+        }
     }
 
 }
