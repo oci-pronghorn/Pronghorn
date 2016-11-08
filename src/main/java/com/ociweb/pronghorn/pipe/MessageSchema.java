756f5726bf2d173b3ede2dbc2facd105f3d483bc
Nathan Tippy <nathan@tippy.name>
2016-11-08 16:40:05 -0600

    added helper method to Schema for looking up high level field locator

--- src/main/java/com/ociweb/pronghorn/pipe/MessageSchema.java
+++ src/main/java/com/ociweb/pronghorn/pipe/MessageSchema.java
@@ -6,11 +6,18 @@ public abstract class MessageSchema {
 
     protected MessageSchema(FieldReferenceOffsetManager from) {
         this.from = from;
-
     }
     
     public static final FieldReferenceOffsetManager from(MessageSchema schema) {
         return schema.from;
+    }
+        
+    public int getLocator(String messageName, String fieldName) {
+    	return from.getLoc(messageName, fieldName);
+    }
+
+    public int getLocator(long messageId, long fieldId) {
+    	return from.getLoc(messageId, fieldId);
     }
     
 }
