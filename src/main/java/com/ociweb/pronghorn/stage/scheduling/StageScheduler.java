8d3ee99252231c769b8f56ea85bc88bf2635f087
Nathan Tippy <nathan@tippy.name>
2017-09-15 16:07:10 -0500

    added default scheduler construction

--- src/main/java/com/ociweb/pronghorn/stage/scheduling/StageScheduler.java
+++ src/main/java/com/ociweb/pronghorn/stage/scheduling/StageScheduler.java
@@ -60,6 +60,30 @@ public abstract class StageScheduler {
 	public abstract boolean awaitTermination(long timeout, TimeUnit unit);
 	public abstract void awaitTermination(long timeout, TimeUnit unit, Runnable clean, Runnable dirty);
 	public abstract boolean TerminateNow();
+
+	
+
+	private static int idealThreadCount() {
+		return Runtime.getRuntime().availableProcessors()*4;
+	}
+	
+	public static StageScheduler defaultScheduler(GraphManager gm) {
+		
+		final boolean threadLimitHard = true;//must make this a hard limit or we can saturate the system easily.
+		final int scale = 2;
+		
+		int threadLimit = idealThreadCount();
+		assert(threadLimit>0);
+		final int countStages = GraphManager.countStages(gm);
+		if (threadLimit<=0 && countStages > scale*threadLimit) {
+			//do not allow the ThreadPerStageScheduler to be used, we must group
+			threadLimit = idealThreadCount()*scale;//this must be large so give them a few more
+		}
+		
+		return (threadLimit>=countStages) ?
+				                           new ThreadPerStageScheduler(gm): 
+ 			                               new FixedThreadsScheduler(gm, threadLimit, threadLimitHard);
+	}
 	
 	
 	
