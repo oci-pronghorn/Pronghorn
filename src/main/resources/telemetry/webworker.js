1bbdbb7a52b7133de113302793402eb4b7299f78
Chandika Guruge <gurugecl@gmail.com>
2017-10-12 14:23:29 -0500

    increased timeout to 2000ms

--- src/main/resources/telemetry/webworker.js
+++ src/main/resources/telemetry/webworker.js
@@ -1,17 +1,29 @@
 self.importScripts('viz-lite.js');
 
-function httpGet(theUrl) {
-    var xmlHttp = null;
-
-    xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", theUrl, false );
-    xmlHttp.send( null );
-
-    return xmlHttp.responseText;
+function httpGet(theUrl, onSuccess, onError) {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.timeout = 2000;
+    xmlHttp.onreadystatechange = function() {
+        if (this.readyState === 4) {
+            if (this.status >= 200 && this.status < 300) {
+                onSuccess(this.responseText);
+            } else{
+                onError("Received " + this.status + " from server.");
+            }
+        }
+    };
+    xmlHttp.open( "GET", theUrl, true );
+    xmlHttp.send();
 }
 
-this.onmessage = function (e) {
-    var dot = httpGet("graph.dot");
-    var updateDemo = Viz(dot, {format: "svg", engine: "dot"});
-    this.postMessage({result: updateDemo});
+var worker = this;
+
+this.onmessage = function onmessage(){
+    httpGet('graph.dot', function(result){
+        var updateDemo = Viz(result, {format: "svg", engine: "dot"});
+        worker.postMessage({result: updateDemo});
+    }, function(error) {
+        console.log('Error: ' + error);
+    })
 };
+
