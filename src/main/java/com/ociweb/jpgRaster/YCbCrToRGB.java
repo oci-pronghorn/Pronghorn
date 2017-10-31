b4a49cd4a38dc83d65d681b1ef43412ba017a423
dannye <corrnondacqb@yahoo.com>
2017-10-31 16:42:01 -0500

    First prototype of YCbCr to RGB conversion

--- src/main/java/com/ociweb/jpgRaster/YCbCrToRGB.java
+++ src/main/java/com/ociweb/jpgRaster/YCbCrToRGB.java
@@ -1,5 +1,17 @@
 package com.ociweb.jpgRaster;
 
 public class YCbCrToRGB {
-
+	public class RGB {
+		public int r;
+		public int g;
+		public int b;
+	}
+	
+	public RGB convertToRGB(int Y, int Cb, int Cr) {
+		RGB rgb = new RGB();
+		rgb.r = (int)Math.min(Math.max(0, Math.floor((double)Y + 1.402 * ((double)Cr - 128) + 0.5)), 255);
+		rgb.g = (int)Math.min(Math.max(0, Math.floor((double)Y - (0.114 * 1.772 * ((double)Cb - 128) + 0.299 * 1.402 * ((double)Cr - 128)) / 0.587 + 0.5)), 255);
+		rgb.b = (int)Math.min(Math.max(0, Math.floor((double)Y + 1.772 * ((double)Cb - 128) + 0.5)), 255);
+		return rgb;
+	}
 }
