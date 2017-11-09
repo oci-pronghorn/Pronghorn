b2dc2aa2eb2dea0c323c66d3768efd2c484fcdd2
che <che@siue.edu>
2017-11-09 11:51:13 -0600

    Add reverseZigZag and readTable

--- src/main/java/com/ociweb/jpgRaster/InverseQuantizer.java
+++ src/main/java/com/ociweb/jpgRaster/InverseQuantizer.java
@@ -1,5 +1,63 @@
 package com.ociweb.jpgRaster;
 
-public class InverseQuantizer {
+import static java.lang.Math.sqrt;
 
+public class InverseQuantizer {
+    int quantizationTable[];
+
+    // TODO populate/read in quantization table
+    void readTable(int[] inputArr, int length) {
+        quantizationTable = new int[length];
+        for (int i = 0; i < length; ++i) {
+            quantizationTable[i] = inputArr[i];
+        }
+    }
+
+    int[][] reverseZigZag(int[] inputArr, int length) {
+        final int EAST = 0;
+        final int SOUTH = 1;
+        final int SOUTHWEST = 2;
+        final int NORTHEAST = 3;
+        double rawSideLength = sqrt(length);
+        int sideLength = (int) rawSideLength;
+        if (sideLength != rawSideLength) {
+            sideLength += 1;
+        }
+        int[][] result = new int[sideLength][sideLength];
+        int insert_i = 0;
+        int insert_j = 0;
+        int direction = SOUTHWEST;
+        for (int i = 0; i < length; ++i) {
+            result[insert_i][insert_j] = inputArr[i];
+            switch (direction) {
+                case SOUTHWEST:
+                    if (insert_j == sideLength - 1) {
+                        insert_i++;
+                        direction = NORTHEAST;
+                    } else if (insert_i == 0) {
+                        insert_j++;
+                        direction = NORTHEAST;
+                    } else {
+                        insert_i--;
+                        insert_j++;
+                    }
+                    break;
+                case NORTHEAST:
+                    if (insert_i == sideLength - 1) {
+                        insert_j++;
+                        direction = SOUTHWEST;
+                    } else if (insert_j == 0) {
+                        insert_i++;
+                        direction = SOUTHWEST;
+                    } else {
+                        insert_i++;
+                        insert_j--;
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+        return result;
+    }
 }
