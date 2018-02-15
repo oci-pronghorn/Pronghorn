c50865c349e52e678f3c26da344e032596f6fe1a
Nathan Tippy <nathantippy@gmail.com>
2018-02-15 14:14:39 -0600

    restore the 1.8 tests

--- src/test/java/com/ociweb/json/encode/JSONRootBoolTests.java
+++ src/test/java/com/ociweb/json/encode/JSONRootBoolTests.java
@@ -29,43 +29,43 @@ public class JSONRootBoolTests {
         assertEquals("null", out.toString());
     }
 
-//    @Test
-//    public void testRootBoolTrue() {
-//        JSONRenderer<Boolean> json = new JSONRenderer<Boolean>()
-//                .bool(o -> o);
-//        json.render(out, true);
-//        assertEquals("true", out.toString());
-//    }
-//
-//    @Test
-//    public void testRootBoolFalse() {
-//        JSONRenderer<Boolean> json = new JSONRenderer<Boolean>()
-//                .bool(o -> o);
-//        json.render(out, false);
-//        assertEquals("false", out.toString());
-//    }
-//
-//    @Test
-//    public void testRootBoolNul_lNull() {
-//        JSONRenderer<Boolean> json = new JSONRenderer<Boolean>()
-//                .nullableBool((o, v) -> v.visit(o != null ? o : false, o == null));
-//        json.render(out, null);
-//        assertEquals("null", out.toString());
-//    }
-//
-//    @Test
-//    public void testRootBoolNull_True() {
-//        JSONRenderer<Boolean> json = new JSONRenderer<Boolean>()
-//                .nullableBool((o, v) -> v.visit(o != null ? o : false, o == null));
-//        json.render(out, true);
-//        assertEquals("true", out.toString());
-//    }
-//
-//    @Test
-//    public void testRootBoolNull_False() {
-//        JSONRenderer<Boolean> json = new JSONRenderer<Boolean>()
-//                .nullableBool((o, v) -> v.visit(o != null ? o : false, o == null));
-//        json.render(out, false);
-//        assertEquals("false", out.toString());
-//    }
+    @Test
+    public void testRootBoolTrue() {
+        JSONRenderer<Boolean> json = new JSONRenderer<Boolean>()
+                .bool(o -> o);
+        json.render(out, true);
+        assertEquals("true", out.toString());
+    }
+
+    @Test
+    public void testRootBoolFalse() {
+        JSONRenderer<Boolean> json = new JSONRenderer<Boolean>()
+                .bool(o -> o);
+        json.render(out, false);
+        assertEquals("false", out.toString());
+    }
+
+    @Test
+    public void testRootBoolNul_lNull() {
+        JSONRenderer<Boolean> json = new JSONRenderer<Boolean>()
+                .nullableBool((o, v) -> v.visit(o != null ? o : false, o == null));
+        json.render(out, null);
+        assertEquals("null", out.toString());
+    }
+
+    @Test
+    public void testRootBoolNull_True() {
+        JSONRenderer<Boolean> json = new JSONRenderer<Boolean>()
+                .nullableBool((o, v) -> v.visit(o != null ? o : false, o == null));
+        json.render(out, true);
+        assertEquals("true", out.toString());
+    }
+
+    @Test
+    public void testRootBoolNull_False() {
+        JSONRenderer<Boolean> json = new JSONRenderer<Boolean>()
+                .nullableBool((o, v) -> v.visit(o != null ? o : false, o == null));
+        json.render(out, false);
+        assertEquals("false", out.toString());
+    }
 }
