3694461488adbc2e51d2638fbe21720f019446b3
David Giovannini <giovanninid@objectcomputing.com>
2018-03-21 10:37:23 -0500

    delete file

--- src/main/java/com/ociweb/json/encode/function/IterMemberFunction.java
+++
@@ -1,7 +0,0 @@
-package com.ociweb.json.encode.function;
-
-// When used as the iterator, N==M, return null to stop, previous N passed in
-//@FunctionalInterface
-public interface IterMemberFunction<T, N, M> {
-    M get(T obj, int i, N node);
-}
