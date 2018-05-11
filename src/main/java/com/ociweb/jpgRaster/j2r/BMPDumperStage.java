oci-pronghorn/Pronghorn

commit 028f3fe2131b5f5865a7a45651083b5a81f407b7

fixed if statements

src/main/java/com/ociweb/jpgRaster/j2r/BMPDumperStage.java
@@ -227,11 +227,13 @@ public class BMPDumperStage extends PronghornStage {
 						else {
 							header.filename = header.filename.substring(0, extension) + ".bmp";
 						}
-						if (verbose) 
+						if (verbose) {
 							System.out.println("Writing to '" + header.filename + "'...");
-						dump(pixels, header.filename);
-						if (verbose) 
+							dump(pixels, header.filename);
+						}
+						if (verbose) {
 							System.out.println("Done.");
+						}
 						if (last == 1) {
 							if (time) {
 								timer += (System.nanoTime() - s);

src/main/java/com/ociweb/jpgRaster/j2r/JPGScannerStage.java
@@ -115,8 +115,9 @@ public class JPGScannerStage extends PronghornStage {
 			header.valid = false;
 			return header;
 		}
-		if (verbose) 
+		if (verbose) {
 			System.out.println("Start of Image");
+		}
 		last = (short)(b.get() & 0xFF);
 		current = (short)(b.get() & 0xFF);
 		
@@ -267,8 +268,9 @@ public class JPGScannerStage extends PronghornStage {
 					if (last == 0xFF) {
 						if      (current == JPGConstants.EOI) {
 							decodeScan(header, mcus, numScans);
-							if (verbose)
+							if (verbose) {
 								System.out.println("End of Image");
+							}
 							break;
 						}
 						else if (current == 0x00) {
@@ -314,10 +316,14 @@ public class JPGScannerStage extends PronghornStage {
 				while (true) {
 					last = current;
 					current = (short)(b.get() & 0xFF);
-					if (last == 0xFF) {
-						if      (current == JPGConstants.EOI) {
-							if (verbose) 
+					if (last == 0xFF)
+					{
+						if(current == JPGConstants.EOI)
+						{
+							if (verbose)
+							{
 								System.out.println("End of Image");
+							}
 							break;
 						}
 						else if (current == 0x00) {
@@ -373,8 +379,9 @@ public class JPGScannerStage extends PronghornStage {
 	// decode a whole scan, progressive images only
 	private boolean decodeScan(Header header, ArrayList<MCU> mcus, int numScans) {
 		// decode scan so far
-		if (verbose) 
+		if (verbose) {
 			System.out.println("Decoding a scan of size " + header.imageData.size());
+		}
 		decoder.beginDecode(header);
 
 		MCU mcu1 = null;
@@ -441,8 +448,9 @@ public class JPGScannerStage extends PronghornStage {
 	 * @param	header object representation of JPG header
 	 */
 	private void ReadQuantizationTable(ByteBuffer b, Header header) throws IOException {
-		if (verbose) 
+		if (verbose) {
 			System.out.println("Reading Quantization Tables");
+		}
 		int length = ((b.get() & 0xFF) << 8) + (b.get() & 0xFF);
 		
 		length -= 2;
@@ -495,8 +503,9 @@ public class JPGScannerStage extends PronghornStage {
 			header.valid = false;
 			return;
 		}
-		if (verbose) 
+		if (verbose) {
 			System.out.println("Reading Start of Frame");
+		}
 		int length = ((b.get() & 0xFF) << 8) + (b.get() & 0xFF);
 		
 		header.precision = (short)(b.get() & 0xFF);
@@ -587,8 +596,9 @@ public class JPGScannerStage extends PronghornStage {
 	 * @param	header object representation of JPG header
 	 */
 	private void ReadHuffmanTable(ByteBuffer b, Header header) throws IOException {
-		if (verbose) 
+		if (verbose) {
 			System.out.println("Reading Huffman Tables");
+		}
 		int length = ((b.get() & 0xFF) << 8) + (b.get() & 0xFF);
 		
 		length -= 2;
@@ -650,8 +660,9 @@ public class JPGScannerStage extends PronghornStage {
 			header.valid = false;
 			return;
 		}
-		if (verbose) 
+		if (verbose) {
 			System.out.println("Reading Start of Scan");
+		}
 		int length = ((b.get() & 0xFF) << 8) + (b.get() & 0xFF);
 		
 		for (int i = 0; i < header.numComponents; ++i) {
@@ -707,8 +718,9 @@ public class JPGScannerStage extends PronghornStage {
 	}
 	
 	private void ReadRestartInterval(ByteBuffer b, Header header) throws IOException {
-		if (verbose) 
+		if (verbose) {
 			System.out.println("Reading Restart Interval");
+		}
 		int length = ((b.get() & 0xFF) << 8) + (b.get() & 0xFF);
 		
 		header.restartInterval = ((b.get() & 0xFF) << 8) + (b.get() & 0xFF);
@@ -719,14 +731,16 @@ public class JPGScannerStage extends PronghornStage {
 	}
 	
 	private void ReadRSTN(ByteBuffer b, Header header) throws IOException {
-		if (verbose) 
+		if (verbose) {
 			System.out.println("Reading RSTN");
+		}
 		// RSTN has no length
 	}
 	
 	private void ReadAPPN(ByteBuffer b, Header header) throws IOException {
-		if (verbose) 
+		if (verbose) {
 			System.out.println("Reading APPN");
+		}
 		int length = ((b.get() & 0xFF) << 8) + (b.get() & 0xFF);
 		
 		// all of APPN markers can be ignored
@@ -736,8 +750,9 @@ public class JPGScannerStage extends PronghornStage {
 	}
 	
 	private void ReadComment(ByteBuffer b, Header header) throws IOException {
-		if (verbose) 
+		if (verbose) {
 			System.out.println("Reading Comment");
+		}
 		int length = ((b.get() & 0xFF) << 8) + (b.get() & 0xFF);
 		
 		// all comment markers can be ignored
@@ -896,8 +911,9 @@ public class JPGScannerStage extends PronghornStage {
 				}
 				if (PipeWriter.tryWriteFragment(output, JPGSchema.MSG_HEADERMESSAGE_1)) {
 					// write header to pipe
-					if (verbose) 
+					if (verbose) {
 						System.out.println("JPG Scanner writing header to pipe...");
+					}
 					PipeWriter.writeInt(output, JPGSchema.MSG_HEADERMESSAGE_1_FIELD_HEIGHT_101, header.height);
 					PipeWriter.writeInt(output, JPGSchema.MSG_HEADERMESSAGE_1_FIELD_WIDTH_201, header.width);
 					PipeWriter.writeASCII(output, JPGSchema.MSG_HEADERMESSAGE_1_FIELD_FILENAME_301, file);
@@ -914,8 +930,9 @@ public class JPGScannerStage extends PronghornStage {
 				// write color component data to pipe
 				for (int i = 0; i < header.numComponents; ++i) {
 					if (PipeWriter.tryWriteFragment(output, JPGSchema.MSG_COLORCOMPONENTMESSAGE_2)) {
-						if (verbose) 
+						if (verbose) {
 							System.out.println("JPG Scanner writing color component to pipe...");
+						}
 						PipeWriter.writeInt(output, JPGSchema.MSG_COLORCOMPONENTMESSAGE_2_FIELD_COMPONENTID_102, header.colorComponents[i].componentID);
 						PipeWriter.writeInt(output, JPGSchema.MSG_COLORCOMPONENTMESSAGE_2_FIELD_HORIZONTALSAMPLINGFACTOR_202, header.colorComponents[i].horizontalSamplingFactor);
 						PipeWriter.writeInt(output, JPGSchema.MSG_COLORCOMPONENTMESSAGE_2_FIELD_VERTICALSAMPLINGFACTOR_302, header.colorComponents[i].verticalSamplingFactor);
@@ -931,8 +948,9 @@ public class JPGScannerStage extends PronghornStage {
 				for (int i = 0; i < header.quantizationTables.length; ++i) {
 					if (header.quantizationTables[i] != null) {
 						if (PipeWriter.tryWriteFragment(output, JPGSchema.MSG_QUANTIZATIONTABLEMESSAGE_3)) {
-							if (verbose) 
+							if (verbose) {
 								System.out.println("JPG Scanner writing quantization table to pipe...");
+							}
 	
 							DataOutputBlobWriter<JPGSchema> quantizationTableWriter = PipeWriter.outputStream(output);
 							DataOutputBlobWriter.openField(quantizationTableWriter);

src/main/java/com/ociweb/jpgRaster/r2j/HuffmanEncoderStage.java
@@ -127,7 +127,9 @@ public class HuffmanEncoderStage extends PronghornStage {
 					break;
 				}
 			}
-			if (found) break;
+			if (found) {
+				break;
+			}
 		}
 		
 		// code AC values
@@ -165,7 +167,9 @@ public class HuffmanEncoderStage extends PronghornStage {
 							break;
 						}
 					}
-					if (found) break;
+					if (found) {
+						break;
+					}
 				}
 				numZeroes -= 16;
 			}
@@ -193,7 +197,7 @@ public class HuffmanEncoderStage extends PronghornStage {
 						break;
 					}
 				}
-				if (found) break;
+				if (found){ break;}
 			}
 		}
 		return true;

src/main/java/com/ociweb/jpgRaster/j2r/HuffmanDecoder.java
@@ -128,10 +128,16 @@ public class HuffmanDecoder {
 											  Header header) {
 		if (progressive) {
 			int componentsInUse = 0;
-			if (header.colorComponents[0].used) ++componentsInUse;
+			if (header.colorComponents[0].used) {
+				++componentsInUse;
+			}
 			if (header.numComponents > 1) {
-				if (header.colorComponents[1].used) ++componentsInUse;
-				if (header.colorComponents[2].used) ++componentsInUse;
+				if (header.colorComponents[1].used) {
+					++componentsInUse;
+				}
+				if (header.colorComponents[2].used) {
+					++componentsInUse;
+				}
 			}
 			
 			if (header.startOfSelection > header.endOfSelection) {
@@ -159,7 +165,9 @@ public class HuffmanDecoder {
 					return false;
 				}
 				short coeff = (short)b.nextBits(length);
-				if (coeff == -1) return true;
+				if (coeff == -1) {
+					return true;
+				}
 				if (length != 0 && coeff < (1 << (length - 1))) {
 					coeff -= (1 << length) - 1;
 				}
@@ -200,7 +208,9 @@ public class HuffmanDecoder {
 						}
 						
 						short coeff = (short)b.nextBits(coeffLength);
-						if (coeff == -1) return true;
+						if (coeff == -1) {
+							return true;
+						}
 						if (coeff < (1 << (coeffLength - 1))) {
 							coeff -= (1 << coeffLength) - 1;
 						}
@@ -310,7 +320,9 @@ public class HuffmanDecoder {
 				return false;
 			}
 			short coeff = (short)b.nextBits(length);
-			if (coeff == -1) return true;
+			if (coeff == -1) {
+				return true;
+			}
 			if (length != 0 && coeff < (1 << (length - 1))) {
 				coeff -= (1 << length) - 1;
 			}
@@ -355,7 +367,9 @@ public class HuffmanDecoder {
 						}
 						
 						coeff = (short)b.nextBits(coeffLength);
-						if (coeff == -1) return true;
+						if (coeff == -1) {
+							return true;
+						}
 						if (coeff < (1 << (coeffLength - 1))) {
 							coeff -= (1 << coeffLength) - 1;
 						}
@@ -368,7 +382,9 @@ public class HuffmanDecoder {
 	}
 	
 	public boolean decodeHuffmanData(MCU mcu1, MCU mcu2, MCU mcu3, MCU mcu4) {
-		if (!b.hasBits()) return true;
+		if (!b.hasBits()) {
+			return true;
+		}
 		
 		int horizontal = header.colorComponents[0].horizontalSamplingFactor;
 		int vertical = header.colorComponents[0].verticalSamplingFactor;

src/main/java/com/ociweb/jpgRaster/j2r/InverseDCTStage.java
@@ -148,8 +148,9 @@ public class InverseDCTStage extends PronghornStage {
 
 				// write color component data to pipe
 				if (PipeWriter.tryWriteFragment(output, JPGSchema.MSG_COLORCOMPONENTMESSAGE_2)) {
-					if (verbose) 
+					if (verbose) {
 						System.out.println("Inverse DCT writing color component to pipe...");
+					}
 					PipeWriter.writeInt(output, JPGSchema.MSG_COLORCOMPONENTMESSAGE_2_FIELD_COMPONENTID_102, component.componentID);
 					PipeWriter.writeInt(output, JPGSchema.MSG_COLORCOMPONENTMESSAGE_2_FIELD_HORIZONTALSAMPLINGFACTOR_202, component.horizontalSamplingFactor);
 					PipeWriter.writeInt(output, JPGSchema.MSG_COLORCOMPONENTMESSAGE_2_FIELD_VERTICALSAMPLINGFACTOR_302, component.verticalSamplingFactor);

src/main/java/com/ociweb/jpgRaster/j2r/InverseQuantizerStage.java
@@ -77,8 +77,9 @@ public class InverseQuantizerStage extends PronghornStage {
 
 				// write header to pipe
 				if (PipeWriter.tryWriteFragment(output, JPGSchema.MSG_HEADERMESSAGE_1)) {
-					if (verbose) 
+					if (verbose) {
 						System.out.println("Inverse Quantizer writing header to pipe...");
+					}
 					PipeWriter.writeInt(output, JPGSchema.MSG_HEADERMESSAGE_1_FIELD_HEIGHT_101, header.height);
 					PipeWriter.writeInt(output, JPGSchema.MSG_HEADERMESSAGE_1_FIELD_WIDTH_201, header.width);
 					PipeWriter.writeASCII(output, JPGSchema.MSG_HEADERMESSAGE_1_FIELD_FILENAME_301, header.filename);
@@ -103,8 +104,9 @@ public class InverseQuantizerStage extends PronghornStage {
 				
 				// write color component data to pipe
 				if (PipeWriter.tryWriteFragment(output, JPGSchema.MSG_COLORCOMPONENTMESSAGE_2)) {
-					if (verbose) 
+					if (verbose) {
 						System.out.println("Inverse Quantizer writing color component to pipe...");
+					}
 					PipeWriter.writeInt(output, JPGSchema.MSG_COLORCOMPONENTMESSAGE_2_FIELD_COMPONENTID_102, component.componentID);
 					PipeWriter.writeInt(output, JPGSchema.MSG_COLORCOMPONENTMESSAGE_2_FIELD_HORIZONTALSAMPLINGFACTOR_202, component.horizontalSamplingFactor);
 					PipeWriter.writeInt(output, JPGSchema.MSG_COLORCOMPONENTMESSAGE_2_FIELD_VERTICALSAMPLINGFACTOR_302, component.verticalSamplingFactor);

src/main/java/com/ociweb/jpgRaster/j2r/YCbCrToRGBStage.java
@@ -60,12 +60,24 @@ public class YCbCrToRGBStage extends PronghornStage {
 		r = (short)(Y + 1.402 * Cr + 128);
 		g = (short)((Y - (0.114 * (Y + 1.772 * Cb)) - 0.299 * (Y + 1.402 * Cr)) / 0.587 + 128);
 		b = (short)(Y + 1.772 * Cb + 128);
-		if (r < 0)   r = 0;
-		if (r > 255) r = 255;
-		if (g < 0)   g = 0;
-		if (g > 255) g = 255;
-		if (b < 0)   b = 0;
-		if (b > 255) b = 255;
+		if (r < 0) {
+			r = 0;
+		}
+		if (r > 255) {
+			r = 255;
+		}
+		if (g < 0) {
+			g = 0;
+		}
+		if (g > 255) {
+			g = 255;
+		}
+		if (b < 0) {
+			b = 0;
+		}
+		if (b > 255) {
+			b = 255;
+		}
 		rgb[0] = (byte)r;
 		rgb[1] = (byte)g;
 		rgb[2] = (byte)b;
@@ -260,8 +272,9 @@ public class YCbCrToRGBStage extends PronghornStage {
 
 				// write header to pipe
 				if (PipeWriter.tryWriteFragment(output, JPGSchema.MSG_HEADERMESSAGE_1)) {
-					if (verbose) 
+					if (verbose) {
 						System.out.println("YCbCrToRGB writing header to pipe...");
+					}
 					PipeWriter.writeInt(output, JPGSchema.MSG_HEADERMESSAGE_1_FIELD_HEIGHT_101, header.height);
 					PipeWriter.writeInt(output, JPGSchema.MSG_HEADERMESSAGE_1_FIELD_WIDTH_201, header.width);
 					PipeWriter.writeASCII(output, JPGSchema.MSG_HEADERMESSAGE_1_FIELD_FILENAME_301, header.filename);
@@ -286,8 +299,9 @@ public class YCbCrToRGBStage extends PronghornStage {
 				
 				// write color component data to pipe
 				if (PipeWriter.tryWriteFragment(output, JPGSchema.MSG_COLORCOMPONENTMESSAGE_2)) {
-					if (verbose) 
+					if (verbose) {
 						System.out.println("YCbCrToRGB writing color component to pipe...");
+					}
 					PipeWriter.writeInt(output, JPGSchema.MSG_COLORCOMPONENTMESSAGE_2_FIELD_COMPONENTID_102, component.componentID);
 					PipeWriter.writeInt(output, JPGSchema.MSG_COLORCOMPONENTMESSAGE_2_FIELD_HORIZONTALSAMPLINGFACTOR_202, component.horizontalSamplingFactor);
 					PipeWriter.writeInt(output, JPGSchema.MSG_COLORCOMPONENTMESSAGE_2_FIELD_VERTICALSAMPLINGFACTOR_302, component.verticalSamplingFactor);

src/main/java/com/ociweb/jpgRaster/r2j/ForwardDCTStage.java
@@ -116,8 +116,9 @@ public class ForwardDCTStage extends PronghornStage {
 
 				// write header to pipe
 				if (PipeWriter.tryWriteFragment(output, JPGSchema.MSG_HEADERMESSAGE_1)) {
-					if (verbose) 
+					if (verbose) {
 						System.out.println("Forward DCT writing header to pipe...");
+					}
 					PipeWriter.writeInt(output, JPGSchema.MSG_HEADERMESSAGE_1_FIELD_HEIGHT_101, header.height);
 					PipeWriter.writeInt(output, JPGSchema.MSG_HEADERMESSAGE_1_FIELD_WIDTH_201, header.width);
 					PipeWriter.writeASCII(output, JPGSchema.MSG_HEADERMESSAGE_1_FIELD_FILENAME_301, header.filename);

commit 4e33d040d197e8f2217d58387215dd70a2c7fd50

Always produce output file regardless of verbose flag bug introduced in 028f3fe2131b5f5865a7a45651083b5a81f407b7 also fix a few styling inconsistencies

src/main/java/com/ociweb/jpgRaster/j2r/BMPDumperStage.java
@@ -229,8 +229,8 @@ public class BMPDumperStage extends PronghornStage {
 						}
 						if (verbose) {
 							System.out.println("Writing to '" + header.filename + "'...");
-							dump(pixels, header.filename);
 						}
+						dump(pixels, header.filename);
 						if (verbose) {
 							System.out.println("Done.");
 						}

src/main/java/com/ociweb/jpgRaster/j2r/JPGScannerStage.java
@@ -316,12 +316,9 @@ public class JPGScannerStage extends PronghornStage {
 				while (true) {
 					last = current;
 					current = (short)(b.get() & 0xFF);
-					if (last == 0xFF)
-					{
-						if(current == JPGConstants.EOI)
-						{
-							if (verbose)
-							{
+					if (last == 0xFF) {
+						if (current == JPGConstants.EOI) {
+							if (verbose) {
 								System.out.println("End of Image");
 							}
 							break;

src/main/java/com/ociweb/jpgRaster/r2j/HuffmanEncoderStage.java
@@ -197,7 +197,9 @@ public class HuffmanEncoderStage extends PronghornStage {
 						break;
 					}
 				}
-				if (found){ break;}
+				if (found) {
+					break;
+				}
 			}
 		}
 		return true;