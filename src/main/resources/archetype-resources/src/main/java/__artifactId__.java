51e230fd2e8bd2bf8b5ad608b0d1fde1885e09b5
Tobias Schweiger <schweigert@objectcomputing.com>
2018-05-16 11:05:39 -0500

    added more comments, cleanup, and started test for main

--- src/main/resources/archetype-resources/src/main/java/__artifactId__.java
+++ src/main/resources/archetype-resources/src/main/java/__artifactId__.java
@@ -13,35 +13,41 @@ import com.ociweb.pronghorn.util.MainArgs;
 public class ${artifactId}  {
 
 	public static void main(String[] args) {
-		
+
+	    //Get the file path from the arguments
 		String inputFilePath = MainArgs.getOptArg("fileName", "-f", args, "./image.jpg");
 
+		//Create a new GraphManager. The GraphManager is essential and keeps track of stages (nodes) and pipes (edges).
 		GraphManager gm = new GraphManager();
 		
 		populateGraph(gm, inputFilePath);
-		
+
+		//By default, telemetry will be accessible on your public IP : given port
 		gm.enableTelemetry(8089);
-		
+
+		//Actually start the default scheduler.
 		StageScheduler.defaultScheduler(gm).startup();
-		
 	}
 
+    private static void populateGraph(GraphManager gm, String inputFilePath) {
 
-	private static void populateGraph(GraphManager gm, String inputFilePath) {
-				
-		Pipe<RawDataSchema> pipe1= RawDataSchema.instance.newPipe(10, 10_000); //10 chunks each 10K in  size
-		Pipe<RawDataSchema> pipe1A= RawDataSchema.instance.newPipe(20, 20_000); //10 chunks each 10K in  size
-		Pipe<RawDataSchema> pipe1B= RawDataSchema.instance.newPipe(20, 20_000); //10 chunks each 10K in  size
-		
+        //Create 3 pipes uing the RawDataSchema
+        //RawDataSchema has one record, with one variable-length field, that allows you to put bytes in, ideal for
+        //file streaming
+		Pipe<RawDataSchema> pipe1 = RawDataSchema.instance.newPipe(10, 10_000); //10 chunks each 10K in  size
+		Pipe<RawDataSchema> pipe1A = RawDataSchema.instance.newPipe(20, 20_000); //10 chunks each 10K in  size
+		Pipe<RawDataSchema> pipe1B = RawDataSchema.instance.newPipe(20, 20_000); //10 chunks each 10K in  size
 		
 		new FileBlobReadStage(gm, pipe1, inputFilePath); //This stage reads a file
 		
 		//This stage replicates the data to two pipes, great for debugging while passing on the real data.
-		new ReplicatorStage<>(gm, pipe1, pipe1A, pipe1B); 
-		
-		new ConsoleJSONDumpStage(gm, pipe1A); //see all the data at the console.
-		
-		new PipeCleanerStage(gm, pipe1B); //dumps all data which came in 
+		new ReplicatorStage<>(gm, pipe1, pipe1A, pipe1B);
+
+        //see all the data at the console.
+		new ConsoleJSONDumpStage(gm, pipe1A);
+
+        //dumps all data which came in
+		new PipeCleanerStage(gm, pipe1B);
 		
 	}
           
---
+++ src/main/resources/archetype-resources/src/test/java/__artifactId__Test.java
@@ -0,0 +1,15 @@
+package ${package};
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+import ${package}.${artifactId};
+import com.ociweb.pronghorn.pipe.util.build.FROMValidation;
+
+public class ${artifactId}Test {
+
+    @Test
+    public void checkPopulateGraphAndResponse() {
+
+    }
+
+}
