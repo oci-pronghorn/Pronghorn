2f50b1f947a030084b6d33e6fa63e70efb8fa3b5
Nathan Tippy <nathantippy@gmail.com>
2018-05-29 13:56:48 -0500

    update to use new Appendables

--- src/main/java/com/ociweb/pronghorn/components/ingestion/csv/MetaMessagesToCSVStage.java
+++ src/main/java/com/ociweb/pronghorn/components/ingestion/csv/MetaMessagesToCSVStage.java
@@ -4,12 +4,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.ociweb.pronghorn.components.ingestion.metaMessageUtil.MetaMessageDefs;
+import com.ociweb.pronghorn.pipe.DataOutputBlobWriter;
 import com.ociweb.pronghorn.pipe.FieldReferenceOffsetManager;
 import com.ociweb.pronghorn.pipe.Pipe;
 import com.ociweb.pronghorn.pipe.PipeReader;
 import com.ociweb.pronghorn.pipe.RawDataSchema;
 import com.ociweb.pronghorn.stage.PronghornStage;
 import com.ociweb.pronghorn.stage.scheduling.GraphManager;
+import com.ociweb.pronghorn.util.Appendables;
 
 public class MetaMessagesToCSVStage extends PronghornStage {
 
@@ -75,34 +77,32 @@ public class MetaMessagesToCSVStage extends PronghornStage {
 		        			long uint = PipeReader.readInt(stage.inputRing, MetaMessageDefs.UINT32_VALUE_LOC);
 							Pipe outputRing = stage.outputRing;	
 		        			
-							Pipe.validateVarLength(outputRing, 21); //RingBuffer.bytesWorkingHeadPosition(ring)
-		        			
-							int max = 21 + Pipe.getBlobWorkingHeadPosition(outputRing);
-							int len = Pipe.leftConvertLongToASCII(outputRing, 0xFFFFFFFFl & uint, max);
-							Pipe.setBytesWorkingHead(outputRing, Pipe.BYTES_WRAP_MASK&(len + Pipe.getBlobWorkingHeadPosition(outputRing)));
-		        		}	
+							DataOutputBlobWriter out = Pipe.openOutputStream(outputRing);
+							Appendables.appendValue(out, uint);
+							DataOutputBlobWriter.closeLowLevelField(out);		        		}	
 						break;
 	        		case 64: //UInt32 Named	   
 		        		{
 		        			sometimesComma(stage);
 		        			long uint = PipeReader.readInt(stage.inputRing, MetaMessageDefs.NAMEDUINT32_VALUE_LOC);
 							Pipe outputRing = stage.outputRing;
-		        			Pipe.validateVarLength(outputRing, 21);
-							int max = 21 + Pipe.getBlobWorkingHeadPosition(outputRing);
-							int len = Pipe.leftConvertLongToASCII(outputRing, 0xFFFFFFFFl&uint, max);
-							Pipe.setBytesWorkingHead(outputRing, Pipe.BYTES_WRAP_MASK&(len + Pipe.getBlobWorkingHeadPosition(outputRing)));
-		        		}	
+							
+							DataOutputBlobWriter out = Pipe.openOutputStream(outputRing);
+							Appendables.appendValue(out, uint);
+							DataOutputBlobWriter.closeLowLevelField(out);		        		}	
 						break;
 						
 	        		case 1: //Int32	      
 		        		{
+		        		
 		        			sometimesComma(stage);
 							int sint = PipeReader.readInt(stage.inputRing, MetaMessageDefs.INT32_VALUE_LOC);
 							Pipe outputRing = stage.outputRing;
-							Pipe.validateVarLength(outputRing, 12);
-							int max = 12 + Pipe.getBlobWorkingHeadPosition(outputRing);
-							int len = Pipe.leftConvertIntToASCII(outputRing, sint, max);
-							Pipe.setBytesWorkingHead(outputRing, Pipe.BYTES_WRAP_MASK&(len + Pipe.getBlobWorkingHeadPosition(outputRing)));
+							
+							DataOutputBlobWriter out = Pipe.openOutputStream(outputRing);
+							Appendables.appendValue(out, sint);
+							DataOutputBlobWriter.closeLowLevelField(out);
+			
 		        		}
 		        		break;
 	        		case 65: //Int32 Named      
@@ -110,11 +110,11 @@ public class MetaMessagesToCSVStage extends PronghornStage {
 		        			sometimesComma(stage);
 							int sint = PipeReader.readInt(stage.inputRing, MetaMessageDefs.NAMEDINT32_VALUE_LOC);
 							Pipe outputRing = stage.outputRing;
-							Pipe.validateVarLength(outputRing, 12);
-							int max = 12 + Pipe.getBlobWorkingHeadPosition(outputRing);
-							int len = Pipe.leftConvertIntToASCII(outputRing, sint, max);
-							Pipe.setBytesWorkingHead(outputRing, Pipe.BYTES_WRAP_MASK&(len + Pipe.getBlobWorkingHeadPosition(outputRing)));
-		        		}
+							
+							DataOutputBlobWriter out = Pipe.openOutputStream(outputRing);
+							Appendables.appendValue(out, sint);
+							DataOutputBlobWriter.closeLowLevelField(out);
+			      		}
 		        		break;
 		        		
 	        		case 2: //UInt64
@@ -122,22 +122,20 @@ public class MetaMessagesToCSVStage extends PronghornStage {
 		        			sometimesComma(stage);
 							long ulong = PipeReader.readLong(stage.inputRing, MetaMessageDefs.UINT64_VALUE_LOC);
 							Pipe outputRing = stage.outputRing;
-							Pipe.validateVarLength(outputRing, 21);
-							int max = 21 + Pipe.getBlobWorkingHeadPosition(outputRing);
-							int len = Pipe.leftConvertLongToASCII(outputRing, ulong, max); //TODO: find a way to deal with top signed bit on long if it happens
-							Pipe.setBytesWorkingHead(outputRing, Pipe.BYTES_WRAP_MASK&(len + Pipe.getBlobWorkingHeadPosition(outputRing))); 
-		        		}
+							
+							DataOutputBlobWriter out = Pipe.openOutputStream(outputRing);
+							Appendables.appendValue(out, ulong);
+							DataOutputBlobWriter.closeLowLevelField(out);
+						}
 		        		break;
 	        		case 66: //UInt64 Named
 		        		{
 		        			sometimesComma(stage);
 							long ulong = PipeReader.readLong(stage.inputRing, MetaMessageDefs.NAMEDUINT64_VALUE_LOC);
 							Pipe outputRing = stage.outputRing;
-							Pipe.validateVarLength(outputRing, 21);
-							int max = 21 + Pipe.getBlobWorkingHeadPosition(outputRing);
-							int len = Pipe.leftConvertLongToASCII(outputRing, ulong, max);//TODO: find a way to deal with top signed bit on long if it happens
-							Pipe.setBytesWorkingHead(outputRing, Pipe.BYTES_WRAP_MASK&(len + Pipe.getBlobWorkingHeadPosition(outputRing))); 
-							
+							DataOutputBlobWriter out = Pipe.openOutputStream(outputRing);
+							Appendables.appendValue(out, ulong);
+							DataOutputBlobWriter.closeLowLevelField(out);
 		        		}
 		        		break;
 	        				        		
@@ -146,11 +144,9 @@ public class MetaMessagesToCSVStage extends PronghornStage {
 		        			sometimesComma(stage);
 							long slong = PipeReader.readLong(stage.inputRing, MetaMessageDefs.INT64_VALUE_LOC);
 							Pipe outputRing = stage.outputRing;
-							Pipe.validateVarLength(outputRing, 21);
-							int max = 21 + Pipe.getBlobWorkingHeadPosition(outputRing);
-							int len = Pipe.leftConvertLongToASCII(outputRing, slong, max);
-							Pipe.setBytesWorkingHead(outputRing, Pipe.BYTES_WRAP_MASK&(len + Pipe.getBlobWorkingHeadPosition(outputRing)));
-							
+							DataOutputBlobWriter out = Pipe.openOutputStream(outputRing);
+							Appendables.appendValue(out, slong);
+							DataOutputBlobWriter.closeLowLevelField(out);
 		        		}
 	        			break;
 	        		case 67: //Int64 Named
@@ -158,10 +154,9 @@ public class MetaMessagesToCSVStage extends PronghornStage {
 		        			sometimesComma(stage);
 							long slong = PipeReader.readLong(stage.inputRing, MetaMessageDefs.NAMEDINT64_VALUE_LOC);
 							Pipe outputRing = stage.outputRing;
-							Pipe.validateVarLength(outputRing, 21);
-							int max = 21 + Pipe.getBlobWorkingHeadPosition(outputRing);
-							int len = Pipe.leftConvertLongToASCII(outputRing, slong, max);
-							Pipe.setBytesWorkingHead(outputRing, Pipe.BYTES_WRAP_MASK&(len + Pipe.getBlobWorkingHeadPosition(outputRing)));
+							DataOutputBlobWriter out = Pipe.openOutputStream(outputRing);
+							Appendables.appendValue(out, slong);
+							DataOutputBlobWriter.closeLowLevelField(out);
 		        		}
 		        		break;      			
 	        			
