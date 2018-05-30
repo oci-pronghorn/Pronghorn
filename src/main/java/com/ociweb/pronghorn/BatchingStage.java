fd38f7e3488251c1eab2b2ad58365852a098f053
Tobias Schweiger <schweigert@objectcomputing.com>
2018-05-30 11:09:48 -0500

    fixed annotations

--- src/main/java/com/ociweb/pronghorn/BatchingStage.java
+++ src/main/java/com/ociweb/pronghorn/BatchingStage.java
@@ -9,9 +9,10 @@ import com.ociweb.pronghorn.stage.scheduling.GraphManager;
 /**
  * Batches messages based on a limit.
  * @param <T>
+ *
  * @author Nathan Tippy
- * @see <a href="https://github.com/objectcomputing/Pronghorn">Pronghorn</a>
  */
+
 public class BatchingStage<T extends MessageSchema<T>> extends PronghornStage {
 
 	private final int limit;
