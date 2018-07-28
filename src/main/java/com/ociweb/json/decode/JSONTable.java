5273804deccb49678a09bf077fda8b8e97402b12
nate <nate@Enoch>
2018-07-28 12:58:17 -0500

    added better helper methods and deprecated the old ones

--- src/main/java/com/ociweb/json/decode/JSONTable.java
+++ src/main/java/com/ociweb/json/decode/JSONTable.java
@@ -11,15 +11,74 @@ public abstract class JSONTable<P> {
     JSONTable(JSONExtractorImpl extractor) {
         this.extractor = extractor;
     }
+    
+    public <T extends Enum<T>> JSONTable<P> integerField(String extractionPath, T field) {
+    	extractor.newPath(JSONType.TypeInteger, false, null);
+    	extractor.completePath(extractionPath, field.name(), field);    	
+    	return this;
+    }
+    
+    public <T extends Enum<T>> JSONTable<P> stringField(String extractionPath, T field) {
+    	extractor.newPath(JSONType.TypeString, false, null);
+    	extractor.completePath(extractionPath, field.name(), field);    	
+    	return this;
+    }
+    
+    public <T extends Enum<T>> JSONTable<P> decimalField(String extractionPath, T field) {
+    	extractor.newPath(JSONType.TypeDecimal, false, null);
+    	extractor.completePath(extractionPath, field.name(), field);    	
+    	return this;
+    }
+    
+    public <T extends Enum<T>> JSONTable<P> booleanField(String extractionPath, T field) {
+    	extractor.newPath(JSONType.TypeBoolean, false, null);
+    	extractor.completePath(extractionPath, field.name(), field);    	
+    	return this;
+    }
+    
+    public <T extends Enum<T>> JSONTable<P> integerField(boolean isAligned, JSONAccumRule accumRule, String extractionPath, T field) {
+    	extractor.newPath(JSONType.TypeInteger, isAligned, accumRule);
+    	extractor.completePath(extractionPath, field.name(), field);    	
+    	return this;
+    }
+    
+    public <T extends Enum<T>> JSONTable<P> stringField(boolean isAligned, JSONAccumRule accumRule, String extractionPath, T field) {
+    	extractor.newPath(JSONType.TypeString, isAligned, accumRule);
+    	extractor.completePath(extractionPath, field.name(), field);    	
+    	return this;
+    }
+    
+    public <T extends Enum<T>> JSONTable<P> decimalField(boolean isAligned, JSONAccumRule accumRule, String extractionPath, T field) {
+    	extractor.newPath(JSONType.TypeDecimal, isAligned, accumRule);
+    	extractor.completePath(extractionPath, field.name(), field);    	
+    	return this;
+    }
+    
+    public <T extends Enum<T>> JSONTable<P> booleanField(boolean isAligned, JSONAccumRule accumRule, String extractionPath, T field) {
+    	extractor.newPath(JSONType.TypeBoolean, isAligned, accumRule);
+    	extractor.completePath(extractionPath, field.name(), field);    	
+    	return this;
+    }
+    
+    public P finish() {
+        return tableEnded();
+    }
 
+    abstract P tableEnded();
+    
+    //////////////////// remove old APIs below here
+
+    @Deprecated
     public JSONPath<JSONTable<P>> element(JSONType type) {
         return element(type, false, null);
     }
 
+    @Deprecated
     public JSONPath<JSONTable<P>> element(JSONType type, boolean isAligned) {
         return element(type, isAligned, null);
     }
 
+    @Deprecated
     public JSONPath<JSONTable<P>> element(JSONType type, boolean isAligned, JSONAccumRule accumRule) {
         JSONExtractorActive active = extractor.newPath(type, isAligned, accumRule);
         return new JSONPath<JSONTable<P>>(active) {
@@ -30,10 +89,5 @@ public abstract class JSONTable<P> {
         };
     }
 
-    public P finish() {
-        return tableEnded();
-    }
-
-    abstract P tableEnded();
 }
 
